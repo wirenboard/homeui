34e9768750d8f6bf7a0d50d3f4d709b65d889df0
Alexey Ignatov <lexszero@gmail.com>
2016-01-15 11:41:53 +0300

    Add conditional templates in dumbtemplate
    Syntax:
    {{if <expr> <op> <expr>}} if true {{else}} if false {{endif}}
    expr - must be a simple variable reference, like foo.bar
    op - operator
        a == b: equality
        a in b: a `is in array` b
    'else' branch is optional and can be omissed

--- app/scripts/services/dumbtemplate.js
+++ app/scripts/services/dumbtemplate.js
@@ -6,7 +6,11 @@ angular.module("homeuiApp.DumbTemplate", [])
       compile: function (src) {
         // sorry, no actual compilation at the moment
         return function (vars) {
-          return src.replace(/\{\{(?:([^{]*?)\|)?\s*([\w.]+?)\s*(?:\|([^}]*?))?\}\}/g, function (m, prefix, expr, suffix) {
+          var expandVar = function(expr) {
+            var t = expr.match(/^"(.*)"$/);
+            if (t) {
+              return t[1];
+            }
             var parts = expr.split("."),
                 v = vars;
             for (var i = 0; i < parts.length; i++) {
@@ -14,8 +18,26 @@ angular.module("homeuiApp.DumbTemplate", [])
               if (!v)
                 break;
             }
-            return v === undefined || v === null || v === "" ? "" : (prefix || "") + v + (suffix || "");
-          });
+            return v;
+          }
+
+          return src
+            .replace(/\{\{\s*if\s+([\w.]+?|".*?")\s*(==|in)\s*([\w.]+?|".*?")\s*\}\}(.*?)(?:\{\{else\}\}(.*?))?\{\{\s*endif\s*\}\}/g, function (m, left, op, right, ifTrue, ifFalse) {
+              left = expandVar(left);
+              right = expandVar(right);
+              var result = false;
+              if (op == "==") {
+                result = (left == right);
+              }
+              else if (op == "in") {
+                result = (right.indexOf(left) >= 0);
+              }
+              return result ? (ifTrue || "") : (ifFalse || "");
+            })
+            .replace(/\{\{(?:([^{]*?)\|)?\s*([\w.]+?)\s*(?:\|([^}]*?))?\}\}/g, function (m, prefix, expr, suffix) {
+              var v = expandVar(expr);
+              return v === undefined || v === null || v === "" ? "" : (prefix || "") + v + (suffix || "");
+            });
         };
       }
     };
