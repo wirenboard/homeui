32c5e3d90097811e5a8f2bd1dce8c8889d982e20
Ivan Shvedunov <ivan4th@gmail.com>
2016-06-05 19:43:18 +0300

    Add switch-cell.

---
+++ app/scripts/directives/switchcell.js
@@ -0,0 +1,18 @@
+"use strict";
+
+angular.module("homeuiApp")
+  .directive("switchCell", function () {
+    return {
+      restrict: "EA",
+      scope: false,
+      require: "^cell",
+      replace: true,
+      // ng-switch-default
+      // Extra <span> around the template is needed to avoid
+      // "Multiple directives ... asking for new/isolated scope on" error.
+      template: "<span ng-switch on='cell.readOnly' class='cell cell-switch'>" +
+        "<input ng-switch-when='true' type='checkbox' readonly disabled ng-model='cell.value'>" +
+        "<toggle-switch ng-switch-default class='control-value' ng-model='cell.value'></toggle-cell>" +
+        "</span>"
+    };
+  });
