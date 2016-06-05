07514601b2d7dfd4b53de2831adf398de06deda5
Ivan Shvedunov <ivan4th@gmail.com>
2016-06-05 18:36:35 +0300

    Add range-cell.

---
+++ app/scripts/directives/rangecell.js
@@ -0,0 +1,28 @@
+"use strict";
+
+angular.module("homeuiApp")
+  .directive("rangeCell", function () {
+    const DEFAULT_MIN = -1e9, DEFAULT_MAX = 1e9, DEFAULT_STEP = 1e-3;
+    return {
+      restrict: "EA",
+      scope: false,
+      require: "^cell",
+      replace: true,
+      template: "<input class='cell cell-range'" +
+        " ng-readonly='cell.readOnly' type='range'" +
+        " ng-model='cell.value'>",
+      link: ($scope, element, attr, cellCtrl) => {
+        // Make sure min/max/step are initialized to a range that is
+        // broad enough for the range control to initialize correctly.
+        // See https://github.com/angular/angular.js/issues/6726
+        function relayAttr (name, defaultValue) {
+          $scope.$watch(() => cellCtrl.cell[name], newValue => {
+            element.attr(name, cellCtrl.cell[name] === null ? defaultValue : cellCtrl.cell[name]);
+          });
+        }
+        relayAttr("min", DEFAULT_MIN);
+        relayAttr("max", DEFAULT_MAX);
+        relayAttr("step", DEFAULT_STEP);
+      }
+    };
+  });
---
+++ test/spec/directives/rangecell.js
@@ -0,0 +1,49 @@
+"use strict";
+
+describe("Directive: range-cell", () => {
+  var f, DeviceData, container, scope, element;
+  beforeEach(module("homeuiApp"));
+  beforeEach(module("homeuiApp.mqttDirectiveFixture"));
+
+  beforeEach(inject(function (MqttDirectiveFixture, _DeviceData_, $compile) {
+    f = new MqttDirectiveFixture("<range-cell cell='dev2/bar'></range-cell>");
+    DeviceData = _DeviceData_;
+    f.extClient.send("/devices/dev2/controls/bar/meta/type", "range", true, 1);
+    f.extClient.send("/devices/dev2/controls/bar/meta/min", "-1000", true, 1);
+    f.extClient.send("/devices/dev2/controls/bar/meta/max", "1000", true, 1);
+    f.extClient.send("/devices/dev2/controls/bar/meta/step", "10", true, 1);
+    f.extClient.send("/devices/dev2/controls/bar", "123", true, 0);
+    f.$scope.$digest();
+  }));
+
+  function input () {
+    return f.container.find("input[type=range].cell.cell-range:visible");
+  }
+
+  it("should display the value of the cell", () => {
+    expect(input()).toHaveLength(1);
+    expect(input()).toBeVisible();
+    expect(input()).toHaveValue("123");
+  });
+
+  it("it should apply min/max and step values to the slider", () => {
+    expect(input().attr("min")).toBe("-1000");
+    expect(input().attr("max")).toBe("1000");
+    expect(input().attr("step")).toBe("10");
+  });
+
+  it("should display readonly cell values in readonly text field", () => {
+    f.extClient.send("/devices/dev2/controls/bar/meta/readonly", "1", true, 1);
+    f.$scope.$digest();
+    expect(input()).toHaveValue("123");
+    expect(input().prop("readonly")).toBe(true);
+  });
+
+  it("should allow value editing for non-readonly cells", () => {
+    input().val("770").change();
+    f.expectJournal().toEqual([
+      "ext: /devices/dev2/controls/bar/on: [770] (QoS 1)"
+    ]);
+    expect(input()).toHaveValue("770");
+  });
+});
