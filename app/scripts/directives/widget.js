ed85a6c932716cc8d633bb375f58160d9fa2103b
Ivan Shvedunov <ivan4th@gmail.com>
2016-06-15 01:47:05 +0300

    Make widgets editable.

--- app/scripts/directives/widget.js
+++ app/scripts/directives/widget.js
@@ -2,21 +2,108 @@
 
 angular.module("homeuiApp")
   .directive("widget", function (DeviceData) {
+    function cellName (id) {
+      try {
+        return DeviceData.proxy(id).name;
+      } catch (e) {
+        console.error("bad cell id: " + id);
+        return id;
+      }
+    }
+
+    var cellTypeNames = ["any"].concat(DeviceData.cellTypeNames());
+
     class WidgetController {
       constructor ($scope, $element, $attrs) {
+        this.cellType = "any";
+        this.source = {};
+        this.originalSource = {};
+        $scope.$watch(() => this._source(), newSource => {
+          if (!$scope.widgetForm.$visible)
+            this.updateSource();
+        }, true);
+        // add cell upon picker selection
+        $scope.$watch(() => this.newCellId, newCellId => {
+          if (!newCellId || this.source.cells.find(cell => cell.id === newCellId))
+            return;
+          this.source.cells.push({ id: newCellId, name: cellName(newCellId) });
+          this.newCellId = null;
+        });
       }
 
-      get source () {
-        return this._source() || {
-          name: "",
-          compact: true,
-          cells: []
-        };
+      updateSource () {
+        var newSource = this._source();
+        this.source = newSource ? angular.copy(newSource) :
+          {
+            name: "",
+            compact: true,
+            cells: []
+          };
+      }
+
+      get cellTypeNames () {
+        return cellTypeNames;
+      }
+
+      cellTypeFromId (id) {
+        try {
+          return DeviceData.proxy(id).type;
+        } catch (e) {
+          console.error("bad cell id: " + id);
+          return "<error>";
+        }
+      }
+
+      cellTypeFilter () {
+        return this.cellType == "any" ? "" : this.cellType;
+      }
+
+      prepareToEdit () {
+        this.source.cells.forEach(cell => {
+          if (!cell.hasOwnProperty("name") || !cell.name)
+            cell.name = cellName(cell.id);
+        });
+      }
+
+      commit () {
+        if (this._source()) {
+          // clear cell names that are the same as original
+          this.source.cells.forEach(cell => {
+            var oldName = cellName(cell.id),
+                newName = (cell.name || "").replace(/^s+|\s+$/g, "");
+            if (!newName || oldName == newName)
+              delete cell.name;
+          });
+          angular.extend(this._source(), this.source);
+        }
+      }
+
+      cancel () {
+        this.updateSource();
+      }
+
+      checkNonEmpty (value, msg) {
+        if (!/\S/.test(value))
+          return msg;
+        return true;
+      }
+
+      deleteCell (cell) {
+        var index = this.source.cells.indexOf(cell);
+        if (index >= 0)
+          this.source.cells.splice(index, 1);
+      }
+
+      verify () {
+        if (!this.source.cells.length)
+          return "error";
+        return true;
       }
     }
 
     return {
       restrict: "EA",
+      scope: {},
       bindToController: {
         _source: "&source"
       },
--- test/spec/directives/widget.js
+++ test/spec/directives/widget.js
@@ -26,6 +26,11 @@ describe("Directive: widget", () => {
     f.$scope.$digest();
   }));
 
+  afterEach(() => {
+    $(".ui-select-container").remove();
+    f.remove();
+  });
+
   it("should display widget title", () => {
     expect(f.container.find(".widget .panel-heading .widget-name")).toHaveText("Some widget");
   });
@@ -74,6 +79,206 @@ describe("Directive: widget", () => {
     ]);
   });
 
-  // TBD: editability w/cancellation
-  // TBD: don't allow invalid widget to be saved (e.g. with blank / space-only title)
+  function edit () {
+    f.click(".glyphicon-edit");
+  }
+
+  function submit () {
+    f.click("button[type=submit]");
+  }
+
+  it("should support edititing widget name", () => {
+    edit();
+    var nameInput = f.container.find(".panel-heading input[type=text]");
+    expect(nameInput).toExist();
+    expect(nameInput).toHaveValue("Some widget");
+    nameInput.val("Foobar").change();
+
+    // not changed yet
+    expect(f.$scope.widget.name).toBe("Some widget");
+
+    submit();
+    expect(f.$scope.widget.name).toBe("Foobar");
+  });
+
+  it("should not allow blank widget names", () => {
+    edit();
+    f.container.find(".panel-heading input[type=text]").val("").change();
+    submit();
+    expect(f.container.find(".editable-error:visible")).toContainText("Empty widget name is not allowed");
+  });
+
+  it("should make cell names editable when in edit mode", () => {
+    edit();
+    expect(".display-cell").not.toExist();
+    expect(f.container.find("tbody > tr").toArray().map(tr => $(tr).find("input[type=text]").val()))
+      .toEqual([
+        "Temp 1",
+        "Voltage 1"
+      ]);
+    f.container.find("tbody > tr:eq(0) input[type=text]").val("Temperature").change();
+    expect(angular.copy(f.$scope.widget.cells)).toEqual([
+      { id: "dev1/temp1" },
+      { id: "dev1/voltage1" }
+    ]);
+    submit();
+    expect(angular.copy(f.$scope.widget.cells)).toEqual([
+      { id: "dev1/temp1", name: "Temperature" },
+      { id: "dev1/voltage1" }
+    ]);
+  });
+
+  it("should revert cell name to default if it's set to empty", () => {
+    f.$scope.widget.cells[0].name = "Temperature";
+    f.$scope.$digest();
+    edit();
+    expect(f.container.find("tbody > tr").toArray().map(tr => $(tr).find("input[type=text]").val()))
+      .toEqual([
+        "Temperature",
+        "Voltage 1"
+      ]);
+    f.container.find("tbody > tr:eq(0) input[type=text]").val("").change();
+    submit();
+    expect(angular.copy(f.$scope.widget.cells)).toEqual([
+      { id: "dev1/temp1" },
+      { id: "dev1/voltage1" }
+    ]);
+  });
+
+  it("should support adding cells", () => {
+    expect(".ui-select-match").not.toExist();
+    edit();
+    expect(".ui-select-match").toExist();
+    f.container.find(".ui-select-match .ui-select-toggle").click();
+    var el = $(".ui-select-container a.ui-select-choices-row-inner:contains(baz)");
+    expect(el).toExist();
+    el.click();
+    expect(angular.copy(f.$scope.widget.cells)).toEqual([
+      { id: "dev1/temp1" },
+      { id: "dev1/voltage1" }
+    ]);
+    // cell picker must be cleared
+    expect(f.container.find(".ui-select-match")).not.toContainText("dev2 / baz");
+    expect(f.container.find("tbody > tr:eq(2) input[type=text]")).toHaveValue("baz");
+    submit();
+    expect(angular.copy(f.$scope.widget.cells)).toEqual([
+      { id: "dev1/temp1" },
+      { id: "dev1/voltage1" },
+      { id: "dev2/baz" }
+    ]);
+  });
+
+  it("should support filtering cells to be added by type", () => {
+    edit();
+    var typeSelect = f.container.find("select[name=type-filter]");
+    expect(typeSelect).toHaveLength(1);
+    expect(typeSelect.val()).toBe("any");
+    var types = typeSelect.find("option").toArray().map(opt => $(opt).text());
+    expect(types).toContain("value");
+    expect(types).toContain("text");
+    expect(types).toContain("temperature");
+    typeSelect.find("option[value=text]").attr("selected", "selected");
+    typeSelect.find("option[value=any]").removeAttr("selected");
+    typeSelect.change();
+    f.container.find(".ui-select-match .ui-select-toggle").click();
+    expect($(".ui-select-container a.ui-select-choices-row-inner:contains(Dev1)")).not.toExist();
+    expect($(".ui-select-container a.ui-select-choices-row-inner:contains(dev2 / baz)")).toExist();
+
+    typeSelect.find("option[value=any]").attr("selected", "selected");
+    typeSelect.find("option[value=text]").removeAttr("selected");
+    typeSelect.change();
+    expect(typeSelect.val()).toBe("any");
+    expect($(".ui-select-container a.ui-select-choices-row-inner:contains(Dev1)")).toExist();
+    expect($(".ui-select-container a.ui-select-choices-row-inner:contains(dev2 / baz)")).toExist();
+  });
+
+  it("should not add duplicate cells", () => {
+    edit();
+    f.container.find(".ui-select-match .ui-select-toggle").click();
+    var el = $(".ui-select-container a.ui-select-choices-row-inner:contains(Temp 1)");
+    expect(el).toExist();
+    el.click();
+    submit();
+    expect(angular.copy(f.$scope.widget.cells)).toEqual([
+      { id: "dev1/temp1" },
+      { id: "dev1/voltage1" }
+    ]);
+  });
+
+  it("should support cell removal", () => {
+    edit();
+    f.click("tbody > tr:eq(1) button[name=delete]");
+    expect(angular.copy(f.$scope.widget.cells)).toEqual([
+      { id: "dev1/temp1" },
+      { id: "dev1/voltage1" }
+    ]);
+    submit();
+    expect(angular.copy(f.$scope.widget.cells)).toEqual([
+      { id: "dev1/temp1" }
+    ]);
+  });
+
+  it("should not allow saving widget without cells", () => {
+    edit();
+    expect(f.container.find(".widget-error:visible")).not.toBeVisible();
+    f.click("tbody > tr:eq(0) button[name=delete]");
+    f.click("tbody > tr:eq(0) button[name=delete]");
+    expect(f.container.find("button[type=submit]")).toBeDisabled();
+  });
+
+  it("should make it possible to cancel changes made during an edit", () => {
+    edit();
+    f.container.find("tbody > tr:eq(0) input[type=text]").val("Temperature").change();
+    f.click("tbody > tr:eq(1) button[name=delete]");
+    f.container.find(".ui-select-match .ui-select-toggle").click();
+    $(".ui-select-container a.ui-select-choices-row-inner:contains(baz)").click();
+    f.click("button[name=cancel]");
+
+    expect(f.container.find(".panel-heading input[type=text]")).not.toBeVisible();
+    expect(angular.copy(f.$scope.widget.cells)).toEqual([
+      { id: "dev1/temp1" },
+      { id: "dev1/voltage1" }
+    ]);
+    expect(extractCells()).toEqual([
+      { value: "42", units: "°C" },
+      { value: "231", units: "V" }
+    ]);
+  });
+
+  it("should ignore source changes during edit", () => {
+    edit();
+    f.$scope.widget.cells[0].name = "This is ignored";
+    f.$scope.$digest();
+    expect(f.container.find("tbody > tr").toArray().map(tr => $(tr).find("input[type=text]").val()))
+      .toEqual([
+        "Temp 1",
+        "Voltage 1"
+      ]);
+  });
+
+  function ckCompact () {
+    return f.container.find("input[name=compact][type=checkbox]");
+  }
+
+  it("should support toggling compact mode", () => {
+    edit();
+    expect(ckCompact()).toExist();
+    expect(ckCompact()).toBeChecked();
+    ckCompact().click();
+    submit();
+    expect(extractCells()).toEqual([
+      { name: "Temp 1", value: "42", units: "°C" },
+      { name: "Voltage 1", value: "231", units: "V" }
+    ]);
+
+    edit();
+    expect(ckCompact()).toExist();
+    expect(ckCompact()).not.toBeChecked();
+    ckCompact().click();
+    submit();
+    expect(extractCells()).toEqual([
+      { value: "42", units: "°C" },
+      { value: "231", units: "V" }
+    ]);
+  });
 });
