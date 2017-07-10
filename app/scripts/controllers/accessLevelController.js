3e77ea3b356df8022df6ca09533262797959ca3d
ozknemoy <ozk.nemoy@mail.ru>
2017-07-10 20:41:45 +0400

    impl popups in access level

--- app/scripts/controllers/accessLevelController.js
+++ app/scripts/controllers/accessLevelController.js
@@ -3,16 +3,37 @@
  */
 
 export default class accessLevelController {
-    constructor(rolesFactory) {
+    constructor($timeout,rolesFactory) {
         'ngInject';
 
-        this.rolesFactory = rolesFactory;
+        this.$timeout = $timeout;
 
+        this.one = {id: 1, name: 'User'};
+        this.two = {id: 2, name: 'Operator'};
+        this.three = {id: 3, name: 'Administrator'};
+        this.rolesFactory = rolesFactory;
+        this.activeRole = rolesFactory.current.role;
         this.level = '' + rolesFactory.getRole();
+        this.type = {id: +this.level};
     }
 
+    select(newType) {
+        if(newType.id > this.type.id) {
+            if(confirm(`Switch to ${newType.name}?`)) {
+                this.type = newType;
+            } else {
+                // откатываю значение
+                // чтобы не переключалось ставлю пустышку
+                this.level = '';
+                this.$timeout(()=>this.level = '' + this.type.id)
+            }
+        } else {
+            this.type = newType;
+        }
+    }
 
     apply() {
-        this.rolesFactory.setRole(this.level)
+        this.rolesFactory.setRole(this.level);
+        this.activeRole = this.level;
     }
 }
