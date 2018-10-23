54f48d9fa123018a494a0586f6896ac9700aaddd
Pavel N <avedomaru@gmail.com>
2018-10-23 19:47:01 +0300

    added methods to display stateful (show/collapse) devices

--- app/scripts/controllers/devicesController.js
+++ app/scripts/controllers/devicesController.js
@@ -1,17 +1,71 @@
 
 class DevicesCtrl {
-    constructor($scope, $state, DeviceData, handleData, rolesFactory) {
+    constructor($scope, $state, $injector, DeviceData, handleData, rolesFactory) {
         'ngInject';
 
         this.haveRights = rolesFactory.checkRights(rolesFactory.ROLE_TWO);
         if(!this.haveRights) return;
 
         this.$state = $state;
+        // object to keep devicesVisibility in State
+        // fore keeping during the navigation
+        if(!this.$state.devicesVisibility) this.$state.devicesVisibility = {};
+        // if this flag is set, we will change the visibility of devices
+        if(this.$state.devicesVisibility.firstRender === undefined) this.$state.devicesVisibility.firstRender = true;
+        this.getDevicesVisibility = (deviceId) => this.$state.devicesVisibility[deviceId];
+
         this.handleData = handleData;
+
+        // object which contain url params.
+        const stateParams = $injector.get('$stateParams');
+        const deviceIdFromUrl = stateParams.deviceId;
+
+        if(deviceIdFromUrl) this.$state.devicesVisibility.firstRender = true;
+        // if devices count > collapseDevicesAfter,
+        // all devices except 1'st will be folded.
+        const collapseDevicesAfter = 5;
+
+        // dynamic classes for arrow chevron in panel-header
+        $scope.getShevronClass = (deviceId) => {
+          return {
+            'glyphicon-chevron-down': this.getDevicesVisibility(deviceId).isOpen,
+            'glyphicon-chevron-right':  !this.getDevicesVisibility(deviceId).isOpen
+          }
+        }
         $scope.dev = devId => DeviceData.devices[devId];
         $scope.cell = id => DeviceData.cell(id);
-        $scope.deviceIds = () => Object.keys(DeviceData.devices).sort();
+        $scope.deviceIds = () => {
+          const devicesIdsList = Object.keys(DeviceData.devices).sort();
 
+          devicesIdsList.map((deviceId) => {
+            if(!this.$state.devicesVisibility.hasOwnProperty(deviceId)) {
+              this.setDevicesVisibility(deviceId)
+            }
+          })
+
+          // two flags to display what we need to custom show/collapse devices
+          const haveDeviceFromUrl = DeviceData.devices.hasOwnProperty(deviceIdFromUrl);
+          const tooManyDevices = devicesIdsList.length > collapseDevicesAfter;
+
+          if(this.$state.devicesVisibility.firstRender && haveDeviceFromUrl) {
+              devicesIdsList.map((deviceId) => {
+                this.$state.devicesVisibility[deviceId].isOpen = deviceIdFromUrl === deviceId ? true : false;
+              })
+              this.$state.devicesVisibility.firstRender = false;
+          }
+          if(this.$state.devicesVisibility.firstRender && tooManyDevices) {
+              devicesIdsList.map((deviceId, index) => {
+                this.$state.devicesVisibility[deviceId].isOpen = tooManyDevices ? index === 0 ? true : false : true;
+              })
+              this.$state.devicesVisibility.firstRender = false;
+          }
+
+          return devicesIdsList;
+      }
+    }
+
+    setDevicesVisibility(deviceId) {
+      this.$state.devicesVisibility[deviceId] = { isOpen: false }
     }
 
     copy(device, control) {
