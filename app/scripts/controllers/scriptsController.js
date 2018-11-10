06a718a08d9db8a6c16e8caff6d69687b9b1b291
Pavel Rakhmanov <avedomaru@gmail.com>
2018-11-10 13:45:56 +0300

    deleted all unsupported on backend controls (#90)

--- app/scripts/controllers/scriptsController.js
+++ app/scripts/controllers/scriptsController.js
@@ -10,13 +10,10 @@ class ScriptsCtrl {
      get list
      /rpc/v1/wbrules/Editor/List/contactless-uLo93IW6a0 {"id":1,"params":{}}
      get one
-     * /rpc/v1/wbrules/Editor/Load/contactless-Usf4VuT4Ba {"id":2,"params":{"path":"rules.js"}}
+     /rpc/v1/wbrules/Editor/Load/contactless-Usf4VuT4Ba {"id":2,"params":{"path":"rules.js"}}
      save
      /rpc/v1/wbrules/Editor/Save/contactless-Usf4VuT4Ba {"id":3,"params":{"path":"rules.js","content": ....
      */
-
-    this.renamedScripts = [];
-    this.deletedScripts = [];
 
     whenMqttReady().then( ()=>
         EditorProxy.List()
@@ -30,63 +27,12 @@ class ScriptsCtrl {
     }).catch(errors.catch("Error listing the scripts"));
   }
 
-  renameMode(index) {
-    this.renamedScripts[index] = true
-  }
-
-  undoRename(index) {
-    this.renamedScripts[index] = false
-  }
-
-  save(index) {
-    var path = this.scripts[index].virtualPath;
-    if (path == this.scripts[index].tempName) return;
-    // приходится сначала запрашивать скрипт изза отсутствия поля контент в списке скриптов
-    this.EditorProxy.Load({path}).then(script=>{
-      console.log("script",script);
-      this.EditorProxy.Save({
-            path,
-            content: script.content
-          })
-          .then(data=> {
-            // after save
-            this.scripts[index].virtualPath = this.scripts[index].tempName;
-            this.renamedScripts[index] = false
-          })
-          .catch(this.errors.catch("Error saving the file"))
-    })
-  }
-
-  restart(index) {
-    this.EditorProxy.Save({
-      path: this.scripts[index].virtualPath,
-      restart: true
-    })
-  }
-
-  toggleScript(index) {//отключить/включить
-    // force previous value till request is in processing
-    this.scripts[index].processingRequest = true;
-    this.EditorProxy.ChangeState({
-      path: this.scripts[index].virtualPath,
-      state: this.scripts[index].enabled
-    })
-    .then(() => {
-      this.scripts[index].processingRequest = false;
-    }, err => {
-      this.scripts[index].processingRequest = false;
-      console.error("ChangeState error: %o", err);
-      this.scripts[index].enabled = !this.scripts[index].enabled;
-    });
-  }
-
   deleteScript(index) {
     if(confirm('Are you sure?')) {
       this.EditorProxy.Remove({path: this.scripts[index].virtualPath}).then(script=>{
         this.scripts.splice(index,1);
       },err=>alert("error"))
     }
-
   }
 }
 
