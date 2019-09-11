21923020b2e78006d9f60021da21a8bd0f609857
Sergey Safronov <safronov.ser@icloud.com>
2019-09-11 14:59:55 +0300

    Reformat code

--- app/scripts/controllers/dashboardsController.js
+++ app/scripts/controllers/dashboardsController.js
@@ -1,57 +1,65 @@
+'use strict';
+
 class DashboardsCtrl {
-  constructor(uiConfig,rolesFactory) {
-    'ngInject';
     
-    this.roles = rolesFactory;
-    this.uiConfig = uiConfig;
-    this.data = uiConfig.data;
-    // FIXME: make better use of the model
-    this.model = (dashboard) => {this.uiConfig.getDashboard(dashboard.id)};
-  }
+    constructor(uiConfig, rolesFactory) {
+        'ngInject';
 
-//.............................................................................
-  addDashboard() {
-    this.uiConfig.addDashboard();
-  }
+        this.roles = rolesFactory;
+        this.uiConfig = uiConfig;
+        this.data = uiConfig.data;
 
-  addDashboardWithSvg() {
-    this.uiConfig.addDashboardWithSvg();
-  }
+        // FIXME: make better use of the model
+        this.model = (dashboard) => { this.uiConfig.getDashboard(dashboard.id) };
+    }
 
-  deleteDashboard(dashbrd) {
-  this.data.dashboards = this.data.dashboards
-      .filter(dashboard => !(dashboard.name === dashbrd.name && dashboard.id === dashbrd.id));
-  };
+    //.............................................................................
+    addDashboard() {
+        this.uiConfig.addDashboard();
+    }
 
-//.............................................................................
-  checkNonEmpty(value, msg) {
-    if (!/\S/.test(value)) return msg;
-    return true;
-  };
+    addDashboardWithSvg() {
+        this.uiConfig.addDashboardWithSvg();
+    }
 
-//.............................................................................
-  cancel(dashboard) {
-    if (dashboard.isNew)
-      this.model(dashboard).remove();
-  };
+    deleteDashboard(dashbrd) {
+        this.data.dashboards = this.data.dashboards
+            .filter(dashboard => !(dashboard.name === dashbrd.name && dashboard.id === dashbrd.id));
+    }
 
-//.............................................................................
-  checkId(value, dashboard) {
-    var r = this.checkNonEmpty(value, "Empty dashboard id is not allowed");
-    if (r !== true)
-      return r;
-    value = value.replace(/^\s+|\s+$/g, "");
-    return this.data.dashboards.some(function (otherDashboard) {
-      return otherDashboard !== dashboard && otherDashboard.id == value;
-    }) ? "Duplicate dashboard ids are not allowed" : true;
-  };
+    //.............................................................................
+    checkNonEmpty(value, msg) {
+        if (!/\S/.test(value)) {
+            return msg;
+        }
+        return true;
+    }
 
-//.............................................................................
-  afterSave(dashboard) {
-    delete dashboard.isNew;
-  };
+    //.............................................................................
+    cancel(dashboard) {
+        if (dashboard.isNew) {
+            this.model(dashboard).remove();
+        }
+    }
 
-  // TBD: add uiconfig methods to save/revert dashboards
+    //.............................................................................
+    checkId(value, dashboard) {
+        var r = this.checkNonEmpty(value, "Empty dashboard id is not allowed");
+        if (r !== true) {
+            return r;
+        }
+        value = value.replace(/^\s+|\s+$/g, '');
+        return this.data.dashboards.some(function (otherDashboard) {
+            return otherDashboard !== dashboard && otherDashboard.id == value;
+        }) ? 'Duplicate dashboard ids are not allowed' : true;
+    }
+
+    //.............................................................................
+    afterSave(dashboard) {
+        delete dashboard.isNew;
+    }
+
+    // TBD: add uiconfig methods to save/revert dashboards
 }
 
 //-----------------------------------------------------------------------------
