77fc35e6a2ef8e2a20de24e4a24d8e117db48d99
Sergey Safronov <safronov.ser@icloud.com>
2019-09-17 20:57:43 +0300

    Reformat code

--- app/scripts/directives/switchcell.js
+++ app/scripts/directives/switchcell.js
@@ -1,27 +1,29 @@
+'use strict';
+
 import template from './switchcell.html';
 
 function switchCellDirective() {
-  return {
-    restrict: 'EA',
-    scope: false,
-    require: '^cell',
-    replace: true,
-    template,
+    return {
+        restrict: 'EA',
+        scope: false,
+        require: '^cell',
+        replace: true,
+        template,
 
-    link: (scope, element, attrs, cellCtrl) => {
-      scope.$watch(()=> scope._value, (newValue, oldValue) => {
-        if (newValue !== oldValue) {
-          scope.cell.value = scope.cell.extra.invert ? !scope._value : scope._value;
+        link: (scope, element, attrs, cellCtrl) => {
+            scope.$watch(() => scope._value, (newValue, oldValue) => {
+                if (newValue !== oldValue) {
+                    scope.cell.value = scope.cell.extra.invert ? !scope._value : scope._value;
+                }
+            });
+            scope.$watch(() => scope.cell.value, (newValue, oldValue) => {
+                if (newValue !== oldValue) {
+                    scope._value = scope.cell.extra.invert ? !scope.cell.value : scope.cell.value;
+                }
+            });
+            scope._value = scope.cell.extra.invert ? !scope.cell.value : scope.cell.value;
         }
-      });
-      scope.$watch(()=> scope.cell.value, (newValue, oldValue) => {
-        if (newValue !== oldValue) {
-          scope._value = scope.cell.extra.invert ? !scope.cell.value : scope.cell.value;
-        }
-      });
-      scope._value = scope.cell.extra.invert ? !scope.cell.value : scope.cell.value;
-    }
-  };
+    };
 }
 
 export default switchCellDirective;
