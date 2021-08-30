44206952c6a1df2244422feb4cea8a0901cf0be0
pete <petr.krasnoshchekov@wirenboard.ru>
2021-08-30 06:45:37 +0000

    Locale selection interface

--- app/scripts/controllers/webUiController.js
+++ app/scripts/controllers/webUiController.js
@@ -3,11 +3,12 @@
  */
 
 export default class WebUICtrl {
-    constructor(uiConfig, errors, rolesFactory) {
+    constructor(uiConfig, errors, rolesFactory, $window, $translate, tmhDynamicLocale) {
         'ngInject';
 
         this.rolesFactory = rolesFactory;
         this.uiConfig = uiConfig;
+        this.language =  $window.localStorage['language'] || 'en';
 
         uiConfig.whenReady()
             .then((data) => {
@@ -16,6 +17,12 @@ export default class WebUICtrl {
                 this.defaultDashboard = uiConfig.getDashboard(data.defaultDashboardId);
             })
             .catch(errors.catch('Error loading WebUI config'));
+
+        this.changeLanguage = () => {
+            $translate.use(this.language);
+            tmhDynamicLocale.set(this.language);
+            $window.localStorage.setItem('language', this.language);
+        }
     }
 
     changeDefaultDashboard() {
