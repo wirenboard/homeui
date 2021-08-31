47a00380a98ec6a78d8f3c4a02d1bde8931fc7a6
Semen Sokolov <s.sokolov@wirenboard.ru>
2021-08-31 20:58:29 +0300

    options + get requests

--- app/scripts/controllers/diagnosticController.js
+++ app/scripts/controllers/diagnosticController.js
@@ -6,22 +6,24 @@ class DiagnosticCtrl {
 
     $scope.getData = function() {
         console.log(1)
-        alert('Добрый день 1');
 
-        $scope.httpGetAsync("http://127.0.0.1:5000/diagcollect", alert);
+        $scope.httpGetAsync("http://127.0.0.1:5000/diagcollect", console.log , 'OPTIONS');
 
-        alert('Добрый день 2');
+        console.log(1.5)
+
+        $scope.httpGetAsync("http://127.0.0.1:5000/diagcollect", alert, 'GET');
+
         console.log(2)
     }
 
 
-    $scope.httpGetAsync = function(theUrl, callback) {
+    $scope.httpGetAsync = function(theUrl, callback, method) {
         var xmlHttp = new XMLHttpRequest();
         xmlHttp.onreadystatechange = function() {
             if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
                 callback(xmlHttp.responseText);
         }
-        xmlHttp.open("GET", theUrl, true);
+        xmlHttp.open(method, theUrl, true);
         xmlHttp.send(null);
     }
 
