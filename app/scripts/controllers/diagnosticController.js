d813d4215dd00cf53ab98479d10cec97793073f9
Semen Sokolov <s.sokolov@wirenboard.ru>
2021-09-13 15:23:11 +0300

    checking download

--- app/scripts/controllers/diagnosticController.js
+++ app/scripts/controllers/diagnosticController.js
@@ -6,10 +6,11 @@ class DiagnosticCtrl {
     whenMqttReady().then(function () {
       console.log("whenMqttReady");
       console.log(mqttClient.getID())
-      console.log(mqttClient.clientID)
-      console.log(mqttClient.clientId)
       mqttClient.addStickySubscription("/rpc/v1/diag/main/diag/" + mqttClient.getID() + "/+", function(msg) {
-          console.log(msg.payload);
+          console.log(msg.payload["result"]);
+          var btn_download_diag = document.getElementByID('downloadDiag');
+          btn_download_diag.disabled = false;
+          btn_download_diag.value = msg.payload["result"];
         });
     });
 
@@ -18,6 +19,18 @@ class DiagnosticCtrl {
           console.log('{"id":  "'+  mqttClient.getID() + '"}');
           mqttClient.send("/rpc/v1/diag/main/diag/"  + mqttClient.getID(), '{"id":  "'+  mqttClient.getID() + '"}');
     }
+
+    $scope.downloadDiag = function() {
+        btn_download_diag = document.getElementByID('downloadDiag');
+        const link = document.createElement('a');
+        link.setAttribute('target', '_blank');
+        link.setAttribute('href',  btn_download_diag.value);
+        link.setAttribute('download', 'file.zip');
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+    }
+}
 
   }
 }
