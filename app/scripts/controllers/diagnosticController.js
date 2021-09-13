33116631dd5bb7ab241889909b49deec31499ca5
Semen Sokolov <s.sokolov@wirenboard.ru>
2021-09-13 14:35:19 +0300

    finding id

--- app/scripts/controllers/diagnosticController.js
+++ app/scripts/controllers/diagnosticController.js
@@ -4,7 +4,10 @@ class DiagnosticCtrl {
 
 
     whenMqttReady().then(function () {
+      console.log("whenMqttReady");
       console.log(mqttClient.getID())
+      console.log(mqttClient.clientID)
+      console.log(mqttClient.clientId)
       mqttClient.addStickySubscription("/rpc/v1/diag/main/diag/" + mqttClient.getID() + "/+", function(msg) {
           console.log(msg.payload);
         });
