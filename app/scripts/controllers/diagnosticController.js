1550813863682665fc6a42f8c455f34b43ddfd84
Semen Sokolov <s.sokolov@wirenboard.ru>
2021-09-13 20:27:12 +0300

    checking download 10

--- app/scripts/controllers/diagnosticController.js
+++ app/scripts/controllers/diagnosticController.js
@@ -22,7 +22,10 @@ class DiagnosticCtrl {
         const link = document.createElement('a');
         link.setAttribute('target', '_blank');
         console.log($element[0].querySelector('#downloadDiag').value)
-        link.setAttribute('href',    URL.createObjectURL('file:/' + $element[0].querySelector('#downloadDiag').value));
+        var url = window.location.href;
+        url = url.substring(url.indexOf('//') + 2);
+        url = url.substring(0, url.indexOf('/'));
+        link.setAttribute('href', 'http://' + url + $element[0].querySelector('#downloadDiag').value.substring(7));
         link.setAttribute('download', 'file.zip');
         document.body.appendChild(link);
         link.click();
