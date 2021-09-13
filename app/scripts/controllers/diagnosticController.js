03ded4ad91cdff6fed4ed0cfb567aaa802df472b
Semen Sokolov <s.sokolov@wirenboard.ru>
2021-09-13 20:36:17 +0300

    checking download 11

--- app/scripts/controllers/diagnosticController.js
+++ app/scripts/controllers/diagnosticController.js
@@ -6,7 +6,6 @@ class DiagnosticCtrl {
     whenMqttReady().then(function () {
       mqttClient.addStickySubscription("/rpc/v1/diag/main/diag/" + mqttClient.getID() + "/+", function(msg) {
           var path = JSON.parse(msg.payload)["result"];
-          console.log(path);
           $element[0].querySelector('#downloadDiag').disabled = false;
           $element[0].querySelector('#downloadDiag').value = path;
         });
@@ -21,11 +20,10 @@ class DiagnosticCtrl {
     $scope.downloadDiag = function() {
         const link = document.createElement('a');
         link.setAttribute('target', '_blank');
-        console.log($element[0].querySelector('#downloadDiag').value)
         var url = window.location.href;
         url = url.substring(url.indexOf('//') + 2);
         url = url.substring(0, url.indexOf('/'));
-        link.setAttribute('href', 'http://' + url + $element[0].querySelector('#downloadDiag').value.substring(7));
+        link.setAttribute('href', 'http://' + url + $element[0].querySelector('#downloadDiag').value.substring(8));
         link.setAttribute('download', 'file.zip');
         document.body.appendChild(link);
         link.click();
