901abbc2fdaec3587cb2da91d85c3eac05ba8bfb
Semen Sokolov <s.sokolov@wirenboard.ru>
2021-09-14 13:51:17 +0300

    checking button changing 5

--- app/scripts/controllers/diagnosticController.js
+++ app/scripts/controllers/diagnosticController.js
@@ -2,31 +2,33 @@ class DiagnosticCtrl {
   constructor($scope, $timeout, $element, mqttClient, whenMqttReady) {
     'ngInject';
 
-    $scope.collectDataBtn = $element[0].querySelector('#downloadDiag');
+    $scope.downloadDataBtn = $element[0].querySelector('#downloadDiag');
+    $scope.collectDataBtn = $element[0].querySelector('#collectDiag');
 
     whenMqttReady().then(function () {
-      $scope.collectDataBtn.style.visibility="hidden";
+      $scope.downloadDataBtn.style.visibility="hidden";
 
       mqttClient.addStickySubscription("/rpc/v1/diag/main/diag/" + mqttClient.getID() + "/+", function(msg) {
           var path = JSON.parse(msg.payload)["result"];
-          $scope.collectDataBtn.disabled = false;
-          $scope.collectDataBtn.value = path;
-          $scope.collectDataBtn.innerHTML = "Download";
+          $scope.downloadDataBtn.disabled = false;
+          $scope.downloadDataBtn.value = path;
+          $scope.downloadDataBtn.innerHTML = "Download";
         });
     });
 
 
     $scope.getData = function() {
           mqttClient.send("/rpc/v1/diag/main/diag/"  + mqttClient.getID(), '{"id":  "'+  mqttClient.getID() + '"}');
-          $scope.collectDataBtn.style.visibility="visible";
-          $scope.collectDataBtn.innerHTML = "Collecting...";
+          $scope.downloadDataBtn.style.visibility="visible";
+          $scope.collectDataBtn.disabled=true;
+          $scope.downloadDataBtn.innerHTML = "Collecting...";
     }
 
     $scope.downloadDiag = function() {
         var url = window.location.href;
         url = url.substring(url.indexOf('//') + 2);
         url = url.substring(0, url.indexOf('/'));
-        var filename = $scope.collectDataBtn.value.substring(9)
+        var filename = $scope.downloadDataBtn.value.substring(9)
 
         const link = document.createElement('a');
         link.setAttribute('target', '_blank');
