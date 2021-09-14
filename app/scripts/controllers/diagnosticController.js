a737816e5f39ae5c46ec775385a2f475254c7bb9
Semen Sokolov <s.sokolov@wirenboard.ru>
2021-09-14 13:08:11 +0300

    checking button changing 4

--- app/scripts/controllers/diagnosticController.js
+++ app/scripts/controllers/diagnosticController.js
@@ -2,27 +2,31 @@ class DiagnosticCtrl {
   constructor($scope, $timeout, $element, mqttClient, whenMqttReady) {
     'ngInject';
 
+    $scope.collectDataBtn = $element[0].querySelector('#downloadDiag');
 
     whenMqttReady().then(function () {
+      $scope.collectDataBtn.style.visibility="hidden";
+
       mqttClient.addStickySubscription("/rpc/v1/diag/main/diag/" + mqttClient.getID() + "/+", function(msg) {
           var path = JSON.parse(msg.payload)["result"];
-          $element[0].querySelector('#downloadDiag').disabled = false;
-          $element[0].querySelector('#downloadDiag').value = path;
-          $element[0].querySelector('#downloadDiag').innerHTML = "Download";
+          $scope.collectDataBtn.disabled = false;
+          $scope.collectDataBtn.value = path;
+          $scope.collectDataBtn.innerHTML = "Download";
         });
     });
 
 
     $scope.getData = function() {
           mqttClient.send("/rpc/v1/diag/main/diag/"  + mqttClient.getID(), '{"id":  "'+  mqttClient.getID() + '"}');
+          $scope.collectDataBtn.style.visibility="visible";
+          $scope.collectDataBtn.innerHTML = "Collecting...";
     }
 
     $scope.downloadDiag = function() {
         var url = window.location.href;
         url = url.substring(url.indexOf('//') + 2);
         url = url.substring(0, url.indexOf('/'));
-        var filename = $element[0].querySelector('#downloadDiag').value.substring(9)
-        console.log(filename)
+        var filename = $scope.collectDataBtn.value.substring(9)
 
         const link = document.createElement('a');
         link.setAttribute('target', '_blank');
