ed6da0592b2579f5528b3297a7643db565bd6d8d
Semen Sokolov <s.sokolov@wirenboard.ru>
2021-09-16 20:14:10 +0300

    just checking timeout

--- app/scripts/controllers/diagnosticController.js
+++ app/scripts/controllers/diagnosticController.js
@@ -4,42 +4,52 @@ class DiagnosticCtrl {
 
     $scope.downloadDataBtn = $element[0].querySelector('#downloadDiag');
     $scope.collectDataBtn = $element[0].querySelector('#collectDiag');
+    $scope.timePromise;
+    $scope.waitingResponse = false;
 
     whenMqttReady().then(function () {
       $scope.downloadDataBtn.style.visibility="hidden";
 
       mqttClient.addStickySubscription("/rpc/v1/diag/main/diag/" + mqttClient.getID() + "/+", function(msg) {
-          var path = JSON.parse(msg.payload)["result"];
-          $scope.downloadDataBtn.disabled = false;
-          $scope.downloadDataBtn.value = path;
-          $scope.downloadDataBtn.innerHTML = "Download";
-        });
+          if ($scope.waitingResponse) {
+              var path = JSON.parse(msg.payload)["result"];
+              $scope.downloadDataBtn.disabled = false;
+              $scope.downloadDataBtn.value = path;
+              $scope.downloadDataBtn.innerHTML = "Download";
+              $scope.waitingResponse = false;
+              $timeout.cancel($scope.timePromise);
+          }
+      });
     });
 
-
     $scope.getData = function() {
-          mqttClient.send("/rpc/v1/diag/main/diag/"  + mqttClient.getID(), '{"id":  "'+  mqttClient.getID() + '"}');
+          mqttClient.send("/rpc/v1/diag/main/diag/"  + mqttClient.getID(),
+           '{"id":  "'+  mqttClient.getID() + '"}', false, 1);
           $scope.downloadDataBtn.style.visibility="visible";
           $scope.collectDataBtn.disabled=true;
           $scope.downloadDataBtn.innerHTML = "Collecting...";
+          $scope.waitingResponse = true;
+          $scope.timePromise = $timeout(function(){
+                        $scope.waitingResponse = false;
+                        $scope.downloadDataBtn.innerHTML = "Timeout exceed";
+                     }, 10000);
     }
 
     $scope.downloadDiag = function() {
         var url = window.location.href;
         url = url.substring(url.indexOf('//') + 2);
         url = url.substring(0, url.indexOf('/'));
-        var filename = $scope.downloadDataBtn.value.substring(9)
+        var filename = $scope.downloadDataBtn.value.substring(14)
 
         const link = document.createElement('a');
         link.setAttribute('target', '_blank');
         link.setAttribute('href', 'http://' + url + '/' + filename);
         link.setAttribute('download', filename);
-        // TODO: серийник контроллера в имя файла
         document.body.appendChild(link);
         link.click();
         link.remove();
     }
-}
+  }
 }
 
 export default DiagnosticCtrl;
