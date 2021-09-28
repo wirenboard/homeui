2e7d0bf2eee565296e0a0abecaaba25ac8a7c7d6
pete <petr.krasnoshchekov@wirenboard.ru>
2021-09-28 18:03:50 +0500

    Update to new version of RPC

--- app/scripts/controllers/serialMetricsController.js
+++ app/scripts/controllers/serialMetricsController.js
@@ -4,7 +4,24 @@ class SerialMetricsCtrl {
 
         this.SerialMetricsProxy = $injector.get('SerialMetricsProxy');
         this.errors = $injector.get('errors');
-        this.enableSpinner = false;
+        this.enableSpinner = true;
+        this.available = false;
+
+        $injector.get('whenMqttReady')()
+            .then( () => this.SerialMetricsProxy.hasMethod('Load') )
+            .then(result => {
+                this.available = result;
+                if (!result) {
+                    this.enableSpinner = false;
+                    this.errors.catch('serial-metrics.labels.unavailable')();
+                } else {
+                    this.getBusLoad();
+                }
+            })
+            .catch( () => {
+                this.enableSpinner = false;
+                this.errors.catch('serial-metrics.labels.unavailable')(err);
+            });
     }
 
     getBusLoad() {
@@ -17,6 +34,21 @@ class SerialMetricsCtrl {
             this.enableSpinner = false;
             this.errors.catch('serial-metrics.errors.load')(err);
         });
+    }
+
+    formatNames(names) {
+        return names.map(n => n.split('/')[1]).filter(n => n).join(', ');
+    }
+
+    getDeviceName(names) {
+        if (names.length) {
+            var name = names[0].split('/')[0];
+            if (name == 'idle') {
+                return 'serial-metrics.labels.idle';
+            }
+            return name;
+        }
+        return '';
     }
 }
 
