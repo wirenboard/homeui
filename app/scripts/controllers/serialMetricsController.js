a4dee50b7e0d7e00b3e9bfbe10dd4b3779d4d7a4
pete <petr.krasnoshchekov@wirenboard.ru>
2021-10-06 17:31:18 +0500

    wb-mqtt-serial metrics page improvements
      * Total bus load
      * Sorting by clicking on appropriate column's header is added
    
      * wb-mqtt-serial metrics page is removed from left panel

--- app/scripts/controllers/serialMetricsController.js
+++ app/scripts/controllers/serialMetricsController.js
@@ -6,6 +6,8 @@ class SerialMetricsCtrl {
         this.errors = $injector.get('errors');
         this.enableSpinner = true;
         this.available = false;
+        this.sortColumn = undefined;
+        this.sortAsc = false;
 
         $injector.get('whenMqttReady')()
             .then( () => this.SerialMetricsProxy.hasMethod('Load') )
@@ -27,8 +29,39 @@ class SerialMetricsCtrl {
     getBusLoad() {
         this.errors.hideError();
         this.enableSpinner = true;
+
+        var isIdle = (ch) => ch.names && ch.names.length && ch.names[0] == 'idle';
+        var formatNames = (names) => names && names.map(n => n.split('/')[1]).filter(n => n).join(', ');
+
+        var transformChannel = (channel) => {
+            channel.serial = false;
+            channel.device = '';
+            if (channel.names && channel.names.length) {
+                var name = channel.names[0].split('/')[0];
+                if (name == 'wb-mqtt-serial') {
+                    channel.device = 'serial-metrics.labels.serial';
+                    channel.serial = true;
+                    delete channel.i50;
+                    delete channel.i95;
+                } else {
+                    channel.device = name;
+                }
+            }
+            channel.controls = formatNames(channel.names);
+            channel.bl = parseFloat(channel.bl);
+            channel.bl15 = parseFloat(channel.bl15);
+            delete channel.names;
+            return channel;
+        };
+
         this.SerialMetricsProxy.Load().then(result => {
-            this.busLoad = result;
+            this.busLoad = result.map(item => {
+                return {
+                    port: item.port,
+                    idle: item.channels.filter(isIdle),
+                    channels: item.channels.filter(ch => !isIdle(ch)).map(transformChannel)
+                };
+            });
             this.enableSpinner = false;
         }).catch( (err) => {
             this.enableSpinner = false;
@@ -36,19 +69,17 @@ class SerialMetricsCtrl {
         });
     }
 
-    formatNames(names) {
-        return names.map(n => n.split('/')[1]).filter(n => n).join(', ');
-    }
-
-    getDeviceName(names) {
-        if (names.length) {
-            var name = names[0].split('/')[0];
-            if (name == 'idle') {
-                return 'serial-metrics.labels.idle';
-            }
-            return name;
+    sortToggle(column) {
+        if (column != this.sortColumn) {
+            this.sortColumn = column;
+            this.sortAsc = false;
+            return;
         }
-        return '';
+        if (!this.sortAsc) {
+            this.sortAsc = true;
+            return;
+        }
+        this.sortColumn = undefined;
     }
 }
 
