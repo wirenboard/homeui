be07adb453c62182e095f7dac827e36fb8518af2
Semen Sokolov <s.sokolov@wirenboard.ru>
2021-10-11 15:13:33 +0300

    spinner+ message test 2

--- app/scripts/controllers/diagnosticController.js
+++ app/scripts/controllers/diagnosticController.js
@@ -54,6 +54,7 @@ class DiagnosticCtrl {
     whenMqttReady()
     .then( () => DiagnosticProxy.hasMethod('diag') )
     .then(function (result) {
+      console.log(result);
       if (!result) {
           return "-1";
       } else {
