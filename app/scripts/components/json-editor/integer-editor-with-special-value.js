7c2ae012f5f14a5cf487d7b7cf35c45a4c35db16
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2021-12-29 16:43:19 +0500

    Editors are moved to separate files

---
+++ app/scripts/components/json-editor/integer-editor-with-special-value.js
@@ -0,0 +1,35 @@
+'use strict';
+
+import { JSONEditor } from "@json-editor/json-editor";
+
+// The editor is used for poll_interval setting of a channel.
+// poll_interval is edited in table's cell.
+// We want to show empty editors for poll_intervals that are not set, but
+// json-editor sets default values to properties in tables cells.
+// -1 is used as a default value to mark that poll_interval is not set.
+// The editor handles the special case and shows an empty edit
+function makeIntegerEditorWithSpecialValue () {
+    return class extends JSONEditor.defaults.editors["integer"] {
+        build() {
+            super.build();
+            if (this.input) {
+                this.input.setAttribute('size', this.input.getAttribute('placeholder').length);
+            }
+        }
+
+        setValue (value) {
+            if (value != -1) {
+                super.setValue(value);
+            }
+        }
+
+        getValue () {
+            if (!(this.input && this.input.value)) {
+              return undefined;
+            }
+            return super.getValue();
+        }
+    }
+}
+
+export default makeIntegerEditorWithSpecialValue
