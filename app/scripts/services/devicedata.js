ae5b42043e8afed05f68a08df70e91c0ccfe47ae
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2022-01-20 15:26:18 +0500

    Read period miss warning
    'p' from /meta/error indicating missed read period is shown as alert sign near control name

--- app/scripts/services/devicedata.js
+++ app/scripts/services/devicedata.js
@@ -441,6 +441,15 @@ function deviceDataService(mqttClient) {
     }
 
     setError (error) {
+      if (typeof error == "string") {
+        const pos = error.indexOf('p');
+        if (pos !== -1) {
+          this.pollMissError = true
+          error = error.replace('p', '')
+        } else {
+          this.pollMissError = false
+        }
+      }
       this.error = error;
     }
 
@@ -679,6 +688,7 @@ function deviceDataService(mqttClient) {
     get units () { return this.cell.units; }
     get readOnly () { return this.cell.readOnly; }
     get error () { return this.cell.error; }
+    get pollMissError () { return this.cell.pollMissError; }
     get min () { return this.cell.min; }
     get max () { return this.cell.max; }
     get step () { return this.cell.step; }
