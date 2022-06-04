2d61e0af893ab6f0269216c1c8c5aa2f8df619f3
Evgeny Boger <boger@wirenboard.com>
2022-06-04 03:32:59 +0300

    allow to set SVG dashboard ad default

--- app/scripts/controllers/homeController.js
+++ app/scripts/controllers/homeController.js
@@ -6,7 +6,12 @@ class HomeCtrl {
     uiConfig.whenReady()
     .then((data) => {
       if (data.defaultDashboardId) {
-        $state.go('dashboard', {id: data.defaultDashboardId});
+        var dashboard = uiConfig.getDashboard(data.defaultDashboardId);
+        if (dashboard.content.isSvg) {
+          $state.go('dashboard-svg', {id: data.defaultDashboardId});
+        } else {
+          $state.go('dashboard', {id: data.defaultDashboardId});
+        }
       }
     });
   }
