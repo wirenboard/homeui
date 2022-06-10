1e7465b3bca8d452421905f2e6a69dee1433c699
KraPete <86825564+KraPete@users.noreply.github.com>
2022-06-10 15:48:28 +0500

    Advertising notification about testing release is added (#330) (#332)
    The notification can be enabled by setting stableRelease property of HtmlWebpackPlugin in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -303,6 +303,8 @@ module.exports = function makeWebpackConfig() {
             new HtmlWebpackPlugin({
                 filename: './index.html',
                 template: './index.ejs',
+                // Set to true when building for stable release
+                stableRelease: true,
                 chunksSortMode: function (a, b) {
                     var order = ["polyfills", "commons", "libs", "js", "vendor", "main"];
                     return order.indexOf(a.names[0]) - order.indexOf(b.names[0]);
