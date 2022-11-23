cb795a7003d99aef2446514baf97770a0f14200f
Ekaterina Volkova <97599621+ekateluv@users.noreply.github.com>
2022-11-23 14:09:54 +0300

    Add display precision (#372)

--- app/scripts/directives/valuecell.js
+++ app/scripts/directives/valuecell.js
@@ -12,6 +12,27 @@ function valueCellDirective() {
     getUnitsName(c) {
       return this.TranslationService.getUnitsName(c);
     }
+
+    getIntegerValue(c){
+      if (("value" in c) && (c.value !== null) && (c.value !== undefined))
+        return c.value.toString().split('.')[0];
+      return "";
+    }
+
+    getFractionalValue(c){
+      if (("value" in c) && (c.value !== null) && (c.value !== undefined)){
+        if (("step" in c) && (c.step !== null) && (c.step !== undefined)){
+          var digits = c.step.toString().split('.')[1].length;
+          return ("." + c.value.toFixed(digits).split('.')[1]);
+        } else {
+          var fraction = c.value.toString().split('.')[1];
+          if (fraction !== undefined){
+            return ("." + fraction);
+          }
+        }
+      }
+      return "";
+    }
   }
 
   return {
