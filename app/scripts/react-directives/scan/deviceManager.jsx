9c21acf1b49d006c12b9666431b0fda7f8f38677
Nikolay Korotkiy <sikmir@disroot.org>
2023-02-01 14:16:23 +0300

    Minor fixes

--- app/scripts/react-directives/scan/deviceManager.jsx
+++ app/scripts/react-directives/scan/deviceManager.jsx
@@ -89,7 +89,7 @@ function ScanningMessage({port}) {
     <>
       <Spinner/>
       <InfoMessage msg={t('device-manager.labels.scanning', {port})}/>
-      <InfoMessage msg={t('device-manager.labels.scanning-stop')}/>
+      {/*<InfoMessage msg={t('device-manager.labels.scanning-stop')}/>*/}
     </>
   )
 }
@@ -107,8 +107,7 @@ function NewFirmwaresNotice() {
   return (
     <div className='alert alert-warning' role='warning'>
       <i className='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></i>
-      <span> {t('device-manager.labels.firmwares-notice')} </span>
-      <a href='https://wirenboard.com/wiki/WB_Modbus_Devices_Firmware_Update' className='alert-link'>{t('device-manager.labels.firmwares-notice-link')}</a>
+      <a href='https://wirenboard.com/wiki/WB_Modbus_Devices_Firmware_Update' className='alert-link'> {t('device-manager.labels.firmwares-notice-link')}</a>
     </div>
   )
 }
