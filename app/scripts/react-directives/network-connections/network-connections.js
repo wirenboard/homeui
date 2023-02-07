3b233e207a4cbb251e1fbf5e3cc03e9238b2f71c
Nikita Maslov <nikita.maslov@wirenboard.ru>
2023-02-07 17:35:37 +0600

    format

--- app/scripts/react-directives/network-connections/network-connections.js
+++ app/scripts/react-directives/network-connections/network-connections.js
@@ -1,9 +1,7 @@
-'use strict';
-
 import ReactDOM from 'react-dom/client';
+import { autorun } from 'mobx';
 import CreateNetworkConnections from './networkConnections';
 import { NetworksEditor } from './editorStore';
-import { autorun } from 'mobx';
 
 function networkConnectionsDirective(mqttClient, whenMqttReady, ConfigEditorProxy, PageState) {
   'ngInject';
@@ -13,11 +11,9 @@ function networkConnectionsDirective(mqttClient, whenMqttReady, ConfigEditorProx
     scope: {
       path: '=',
     },
-    link: function (scope, element) {
-      scope.onSave = data => {
-        return ConfigEditorProxy.Save({ path: scope.path, content: data });
-      };
-      scope.toggleConnectionState = uuid => {
+    link(scope, element) {
+      scope.onSave = (data) => ConfigEditorProxy.Save({ path: scope.path, content: data });
+      scope.toggleConnectionState = (uuid) => {
         mqttClient.send(`/devices/system__networks__${uuid}/controls/UpDown/on`, '1', false);
       };
 
@@ -26,35 +22,35 @@ function networkConnectionsDirective(mqttClient, whenMqttReady, ConfigEditorProx
       }
       scope.editor = new NetworksEditor(scope.onSave, scope.toggleConnectionState);
       autorun(() => {
-        PageState.setDirty(scope.editor.connections.connections.find(connection => connection.isChanged));
+        PageState.setDirty(scope.editor.connections.connections.find((connection) => connection.isChanged));
       });
       scope.root = ReactDOM.createRoot(element[0]);
       scope.root.render(CreateNetworkConnections({ editor: scope.editor }));
 
       const re = new RegExp('/devices/system__networks__([^/]+)/');
       whenMqttReady().then(() => {
-        ConfigEditorProxy.Load({ path: scope.path }).then(r => {
+        ConfigEditorProxy.Load({ path: scope.path }).then((r) => {
           scope.configPath = r.configPath;
           scope.editor.connections.setSchemaAndData(r.schema, r.content);
           mqttClient
-            .addStickySubscription('/devices/+/controls/State', msg => {
+            .addStickySubscription('/devices/+/controls/State', (msg) => {
               const match = msg.topic.match(re);
               if (match) {
-                var connection = scope.editor.connections.connections.find(
-                  con => con.data.connection_uuid == match[1]
+                const connection = scope.editor.connections.connections.find(
+                  (con) => con.data.connection_uuid == match[1],
                 );
                 if (connection) {
                   connection.setState(msg.payload);
                 }
               }
             })
-            .catch(err => {
+            .catch((err) => {
               scope.editor.connections.error = err.mesage;
             });
         });
       });
 
-      element.on('$destroy', function () {
+      element.on('$destroy', () => {
         scope.root.unmount();
       });
     },
