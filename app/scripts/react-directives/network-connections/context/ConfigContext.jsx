c1cfae3a4f217a8289b9b61cc15528cb5dae95ed
Nikita Maslov <nikita.maslov@wirenboard.ru>
2023-02-07 20:13:28 +0600

    cleanup

--- app/scripts/react-directives/network-connections/context/ConfigContext.jsx
+++ app/scripts/react-directives/network-connections/context/ConfigContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext } from 'react';
-import { observable, action } from 'mobx';
+import { observable } from 'mobx';
 
 const ConfigContext = createContext();
 
@@ -21,9 +21,8 @@ export const ConfigContextData = (onSave) => observable({
 
   saveConnections(connections) {
     onSave({ ui: { connections, con_switcher: this.con_switcher } });
+    self.isDirty = false;
   },
-}, {
-  setConfigData: action,
 });
 
 export function ConfigProvider({ data, children }) {
