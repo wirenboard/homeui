6c01a7b7e01833cb855d1b53a8050625b9301c5e
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2023-02-20 11:42:37 +0500

    Fix mobile view

--- app/scripts/react-directives/network-connections/switcherEditor.jsx
+++ app/scripts/react-directives/network-connections/switcherEditor.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
-import { Button, FormIntegerEdit, FormCheckbox, FormStringEdit, BootstrapRow } from '../common';
+import { Button, FormCheckbox, FormStringEdit, BootstrapRow } from '../common';
 import { DndContext, useDroppable, useDraggable } from '@dnd-kit/core';
 import { restrictToFirstScrollableAncestor } from '@dnd-kit/modifiers';
 import { useTranslation } from 'react-i18next';
+import { useMediaQuery } from 'react-responsive';
 
-const SwitcherElement = ({ connection, moveLeft, moveRight }) => {
+const MobileSwitcherElement = ({ connection, moveLeft, moveRight }) => {
+  const { attributes, listeners, setNodeRef, transform, isDragging } = useDraggable({
+    id: connection.name,
+    data: connection,
+  });
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+      }
+    : undefined;
+  return (
+    <div
+      className={
+        isDragging ? 'priority-item well well-sm is-dragging' : 'priority-item well well-sm'
+      }
+      style={style}
+      ref={setNodeRef}
+    >
+      <div className="priority-item-name" {...listeners} {...attributes}>
+        {connection.name}
+      </div>
+      {moveLeft && (
+        <Button onClick={() => moveLeft(connection)} icon="glyphicon glyphicon-arrow-up" />
+      )}
+      {moveRight && (
+        <Button onClick={() => moveRight(connection)} icon="glyphicon glyphicon-arrow-down" />
+      )}
+    </div>
+  );
+};
+
+const DesktopSwitcherElement = ({ connection, moveLeft, moveRight }) => {
   const { attributes, listeners, setNodeRef, transform, isDragging } = useDraggable({
     id: connection.name,
     data: connection,
@@ -33,6 +65,18 @@ const SwitcherElement = ({ connection, moveLeft, moveRight }) => {
         <Button onClick={() => moveRight(connection)} icon="glyphicon glyphicon-arrow-right" />
       )}
     </div>
+  );
+};
+
+const SwitcherElement = ({ connection, moveLeft, moveRight }) => {
+  const isMobile = useMediaQuery({ maxWidth: 991 });
+  if (isMobile) {
+    return (
+      <MobileSwitcherElement connection={connection} moveLeft={moveLeft} moveRight={moveRight} />
+    );
+  }
+  return (
+    <DesktopSwitcherElement connection={connection} moveLeft={moveLeft} moveRight={moveRight} />
   );
 };
 
