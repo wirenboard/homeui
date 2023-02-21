4caf7e691ef69b9e3318d30c89fc5941ae4c19a6
KraPete <86825564+KraPete@users.noreply.github.com>
2023-02-21 10:00:52 +0500

    Bugfix/refactoring rebase (#402)
    Device manager react components refactoring

--- app/scripts/react-directives/scan/common.jsx
+++ app/scripts/react-directives/scan/common.jsx
@@ -1,23 +1,80 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-function WarningTag({text}) {
-    return <span className='tag bg-warning text-nowrap'>{text}</span>;
+export function WarningTag({ text }) {
+  return <span className="tag bg-warning text-nowrap">{text}</span>;
 }
 
-function ErrorTag({text, title=''}) {
-    return <span className='tag bg-danger text-nowrap' title={title}>{text}</span>;
+export function ErrorTag({ text, title }) {
+  return (
+    <span className="tag bg-danger text-nowrap" title={title}>
+      {text}
+    </span>
+  );
 }
 
-function FirmwareVersionWithLabels({version, availableFw, extSupport}) {
-    const { t } = useTranslation();
-    return (
-        <>
-        {version}
-        {version && availableFw && <WarningTag text={t('device-manager.labels.available', {version: availableFw})}/>}
-        {version && extSupport && <span className="glyphicon glyphicon-flash" title={t('device-manager.labels.extended-modbus')}></span>}
-        </>
-    );
+export const ErrorCheck = ({ errors, errorId, children }) => {
+  const { t } = useTranslation();
+  const error = errors.find(e => e.id === errorId);
+  if (error) {
+    return <ErrorTag text={t('com.wb.device_manager.error')} title={t(errorId) || error.message} />;
+  }
+  return <>{children}</>;
+};
+
+export function FirmwareVersion({ version, availableFw, extSupport, errors }) {
+  const { t } = useTranslation();
+  return (
+    <ErrorCheck errors={errors} errorId={'com.wb.device_manager.device.read_fw_version_error'}>
+      {version}
+      {availableFw && (
+        <WarningTag text={t('device-manager.labels.available', { version: availableFw })} />
+      )}
+      {extSupport && (
+        <span
+          className="glyphicon glyphicon-flash"
+          title={t('device-manager.labels.extended-modbus')}
+        ></span>
+      )}
+    </ErrorCheck>
+  );
 }
 
-export { WarningTag, ErrorTag, FirmwareVersionWithLabels };
+export const DeviceName = ({ title, bootloaderMode, online, poll, errors }) => {
+  const { t } = useTranslation();
+  return (
+    <ErrorCheck
+      errors={errors}
+      errorId={'com.wb.device_manager.device.read_device_signature_error'}
+    >
+      <div className="pull-left">
+        <b>{title}</b>
+      </div>
+      <div className="pull-right">
+        {bootloaderMode && <ErrorTag text={t('device-manager.labels.in-bootloder')} />}
+        {!online && <ErrorTag text={t('device-manager.labels.offline')} />}
+        {!poll && <WarningTag text={t('device-manager.labels.not-polled')} />}
+      </div>
+    </ErrorCheck>
+  );
+};
+
+export const SlaveId = ({ slaveId, isDuplicate }) => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <span className="slave-id">{slaveId}</span>{' '}
+      {isDuplicate && <ErrorTag text={t('device-manager.labels.duplicate')} />}
+    </>
+  );
+};
+
+export const Port = ({ path, baudRate, dataBits, parity, stopBits }) => {
+  return (
+    <>
+      {path} <span className="baudrate">{baudRate}</span> {dataBits.toString()}
+      {parity}
+      {stopBits.toString()}
+    </>
+  );
+};
--- app/scripts/react-directives/scan/mobile.jsx
+++ app/scripts/react-directives/scan/mobile.jsx
@@ -1,129 +1,84 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { WarningTag, ErrorTag, FirmwareVersionWithLabels } from './common';
+import { FirmwareVersion, DeviceName, SlaveId, Port } from './common';
 
-function Tags({bootloader_mode, online, poll}) {
-  const { t } = useTranslation();
+const Row = ({ children }) => {
   return (
-    <div className='pull-right'>
-        {bootloader_mode && <ErrorTag text={t("device-manager.labels.in-bootloder")}/>}
-        {!online && <ErrorTag text={t("device-manager.labels.offline")}/>}
-        {!poll && <WarningTag text={t("device-manager.labels.not-polled")}/>}
+    <div className="row">
+      <div className="col-xs-12">{children}</div>
     </div>
   );
-}
+};
 
-function DeviceName(props) {
-  const { t } = useTranslation();
-  const errorId = "com.wb.device_manager.device.read_device_signature_error";
-  const error = props.errors?.find(e => e.id === errorId);
-  if (error) {
-    return (
-      <div className='row'>
-        <div className='col-xs-12'>
-          <ErrorTag text={t("com.wb.device_manager.error")}/>
-        </div>
-      </div>
-    );
-  }
-  return ( 
-    <div className='row'>
-      <div className='col-xs-12'>
-        <div className='pull-left'>
-            <b>{props.title}</b>
-        </div>
-        <Tags bootloader_mode={props.bootloader_mode} online={props.online} poll={props.poll} />
-      </div>
+const RowWithTitle = ({ title, children }) => {
+  return (
+    <div className="row">
+      <div className="col-xs-3">{title}</div>
+      <div className="col-xs-9">{children}</div>
     </div>
   );
-}
+};
 
-function Row({title, children}) {
-  return (
-    <div className='row'>
-      <div className='col-xs-3'>{title}</div>
-      <div className='col-xs-9'>{children}</div>
-    </div>
-  );
-}
-
-function SlaveId({slaveId, isDuplicate}) {
-  const { t } = useTranslation();
-  return (
-    <Row title={t('device-manager.labels.address')}>
-      {slaveId} {isDuplicate && <ErrorTag text={t('device-manager.labels.duplicate')}/>}
-    </Row>
-  );
-}
-
-function SerialNumber({sn}) {
-  return <Row title='SN'>{sn}</Row>;
-}
-
-function Port({path, baud_rate, data_bits, parity, stop_bits}) {
-  const { t } = useTranslation();
-  return  (
-    <Row title={t('device-manager.labels.port')}>
-      {path} {baud_rate} {data_bits.toString()}{parity}{stop_bits.toString()}
-    </Row>
-  );
-}
-
-function Firmware(props) {
-  const { t } = useTranslation();
-  const errorId = "com.wb.device_manager.device.read_fw_version_error";
-  const error = props.errors.find(e => e.id === errorId);
-  if (error) {
-    return (
-      <Row title={t('device-manager.labels.firmware')}>
-        <ErrorTag text={t("com.wb.device_manager.error")}/>
-      </Row>
-    );
+const Error = ({ error }) => {
+  if (!error) {
+    return;
   }
   return (
-    <Row title={t('device-manager.labels.firmware')}>
-      <FirmwareVersionWithLabels version={props.fw?.version} availableFw={props.fw?.update?.available_fw} extSupport={props.fw?.ext_support}/>
+    <Row>
+      <div className="tag bg-danger error">{error}</div>
     </Row>
   );
-}
+};
 
-function Error({error}) {
+const DevicePanel = props => {
+  const { t } = useTranslation();
   return (
-    <div className='row'>
-      <div className='col-xs-12'>
-        <div className='tag bg-danger error'>
-          {error}
-        </div>
+    <div className="panel panel-default">
+      <div className="panel-body">
+        <Row>
+          <DeviceName
+            title={props.title}
+            bootloaderMode={props.bootloader_mode}
+            online={props.online}
+            poll={props.poll}
+            errors={props.errors}
+          />
+        </Row>
+        <RowWithTitle title="SN">{props.sn}</RowWithTitle>
+        <RowWithTitle title={t('device-manager.labels.address')}>
+          <SlaveId slaveId={props.cfg.slave_id} isDuplicate={props.slave_id_collision} />
+        </RowWithTitle>
+        <RowWithTitle title={t('device-manager.labels.port')}>
+          <Port
+            path={props.port.path}
+            baudRate={props.cfg.baud_rate}
+            dataBits={props.cfg.data_bits}
+            parity={props.cfg.parity}
+            stopBits={props.cfg.stop_bits}
+          />
+        </RowWithTitle>
+        <RowWithTitle title={t('device-manager.labels.firmware')}>
+          <FirmwareVersion
+            version={props.fw?.version}
+            availableFw={props.fw?.update?.available_fw}
+            extSupport={props.fw?.ext_support}
+            errors={props.errors}
+          />
+        </RowWithTitle>
+        <Error error={props?.fw?.update?.error} />
       </div>
     </div>
   );
-}
+};
 
-function DevicePanel(props) {
-    var error;
-    if (props.fw && props.fw.update)
-      error = props.fw.update.error;
-    return (
-      <div key={props.uuid} className='panel panel-default'>
-        <div className='panel-body'>
-          <DeviceName {...props} />
-          <SerialNumber sn={props.sn} />
-          <SlaveId slaveId={props.cfg.slave_id} isDuplicate={props.slave_id_collision}></SlaveId>
-          <Port path={props.port.path} baud_rate={props.cfg.baud_rate} data_bits={props.cfg.data_bits} parity={props.cfg.parity} stop_bits={props.cfg.stop_bits} />
-          <Firmware {...props} />
-          {error && <Error error={error}/>}
-        </div>
-      </div>
-    );
-}
-
-function DevicesList({devices}) {
-    const rows = devices.map((d) => DevicePanel(d));
-    return (
-      <>
-        {rows}
-      </>
-    );
-}
+const DevicesList = ({ devices }) => {
+  return (
+    <>
+      {devices.map(d => (
+        <DevicePanel key={d.uuid} {...d} />
+      ))}
+    </>
+  );
+};
 
 export default DevicesList;
