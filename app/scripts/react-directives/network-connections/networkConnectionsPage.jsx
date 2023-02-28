60da310367dc688bfe0445e0b7e559684669d800
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2023-02-28 17:28:01 +0500

    Do not show deprecation warning during RPC call

--- app/scripts/react-directives/network-connections/networkConnectionsPage.jsx
+++ app/scripts/react-directives/network-connections/networkConnectionsPage.jsx
@@ -76,23 +76,25 @@ const NetworkConnectionsPage = observer(({ pageStore }) => {
     <div className="network-connections-page">
       <ErrorBar msg={pageStore.error}></ErrorBar>
       <h1 className="page-header">{t('network-connections.labels.connections')}</h1>
-      <DeprecationWarning deprecatedConnections={pageStore.connections.deprecatedConnections} />
       <ConfirmModal {...pageStore.confirmModalState} />
       <SelectModal {...pageStore.selectNewConnectionModalState} />
       {pageStore.loading ? (
         <Spinner />
       ) : (
-        <PageTabs
-          connections={pageStore.connections}
-          switcher={pageStore.switcher}
-          selectedIndex={pageStore.selectedTabIndex}
-          onSelect={(index, lastIndex) => pageStore.onSelect(index, lastIndex)}
-          onSelectConnection={index => pageStore.selectConnection(index)}
-          onAdd={() => pageStore.createConnection()}
-          onDeleteConnection={connection => pageStore.deleteConnection(connection)}
-          onToggleConnectionState={connection => pageStore.toggleConnectionState(connection)}
-          onSave={() => pageStore.saveAll()}
-        />
+        <>
+          <DeprecationWarning deprecatedConnections={pageStore.connections.deprecatedConnections} />
+          <PageTabs
+            connections={pageStore.connections}
+            switcher={pageStore.switcher}
+            selectedIndex={pageStore.selectedTabIndex}
+            onSelect={(index, lastIndex) => pageStore.onSelect(index, lastIndex)}
+            onSelectConnection={index => pageStore.selectConnection(index)}
+            onAdd={() => pageStore.createConnection()}
+            onDeleteConnection={connection => pageStore.deleteConnection(connection)}
+            onToggleConnectionState={connection => pageStore.toggleConnectionState(connection)}
+            onSave={() => pageStore.saveAll()}
+          />
+        </>
       )}
     </div>
   );
