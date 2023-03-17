f7c4d64dbee69cee7d3344f016000f0f33db4ab1
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2023-03-17 18:28:27 +0500

    Add option to hide system devices

--- app/scripts/controllers/webUiController.js
+++ app/scripts/controllers/webUiController.js
@@ -7,6 +7,8 @@ export default class WebUICtrl {
         this.rolesFactory = rolesFactory;
         this.uiConfig = uiConfig;
         this.language = $window.localStorage['language'];
+        this.showSystemDevices = $window.localStorage['show-system-devices'] || 'no';
+
         if (!this.language || i18n.languages.indexOf(this.language) === -1) {
             let preferredLanguages = window.navigator.languages.map(lang => lang.split('-')[0]);
             this.language = preferredLanguages.filter(lang => i18n.languages.indexOf(lang) !== -1)[0] || 'en';
@@ -25,6 +27,10 @@ export default class WebUICtrl {
             tmhDynamicLocale.set(this.language);
             $window.localStorage.setItem('language', this.language);
             i18n.changeLanguage(this.language);
+        }
+
+        this.setShowSystemDevices = () => {
+            $window.localStorage.setItem('show-system-devices', this.showSystemDevices);
         }
     }
 
