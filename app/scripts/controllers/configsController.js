5b62cd367f7ea2ff73915dba5b6bd1fa2ffd94a6
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2023-04-03 11:30:17 +0500

    Sort configs alphabetically

--- app/scripts/controllers/configsController.js
+++ app/scripts/controllers/configsController.js
@@ -8,6 +8,12 @@ class ConfigsCtrl {
     $scope.$locale = $locale;
     whenMqttReady().then(() => ConfigEditorProxy.List()).then(result => {
       $scope.configs = result;
+      $scope.configs.sort((a, b) => {
+        const aTitle = (a.titleTranslations && a.titleTranslations[$locale.id]) || a.title;
+        const bTitle = (b.titleTranslations && b.titleTranslations[$locale.id]) || b.title;
+        return aTitle.localeCompare(bTitle);
+      })
+
     }).catch(errors.catch("Error listing the configs"));
   }
 }
