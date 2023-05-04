2def04252d4c53d76a41ad8bc344f70f700fa248
KraPete <86825564+KraPete@users.noreply.github.com>
2023-05-04 12:25:59 +0500

    Recreate json-editor if schema changes (#428)

--- app/scripts/react-directives/network-connections/jsonEditor.jsx
+++ app/scripts/react-directives/network-connections/jsonEditor.jsx
@@ -2,27 +2,39 @@ import { observer } from 'mobx-react-lite';
 import React, { useLayoutEffect, useRef } from 'react';
 import i18n from '../../i18n/react/config';
 import { createJSONEditor } from '../../json-editor/wb-json-editor';
+import { isEqual } from 'lodash';
 
 const JsonEditor = observer(props => {
   const container = useRef();
   var jse = useRef(null);
 
+  const constructEditor = props => {
+    var editor = createJSONEditor(
+      container.current,
+      props.schema,
+      props.data,
+      i18n.language,
+      props.root
+    );
+    editor.on('change', () => {
+      if (props.onChange) {
+        props.onChange(editor.getValue(), editor.validate());
+      }
+    });
+    return editor;
+  };
+
   useLayoutEffect(() => {
     if (!jse.current) {
-      jse.current = createJSONEditor(
-        container.current,
-        props.schema,
-        props.data,
-        i18n.language,
-        props.root
-      );
-      jse.current.on('change', () => {
-        if (props.onChange) {
-          props.onChange(jse.current.getValue(), jse.current.validate());
-        }
-      });
+      jse.current = constructEditor(props);
     } else {
-      jse.current.setValue(props.data);
+      if (isEqual(props.schema, jse.current.schema)) {
+        jse.current.setValue(props.data);
+      } else {
+        jse.current.destroy();
+        jse.current = undefined;
+        jse.current = constructEditor(props);
+      }
     }
   });
   return <div ref={container} />;
