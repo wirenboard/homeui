6c05598e1c3a8f4f99a5d0ca7d416ee4c00ab20c
Nikolay Korotkiy <sikmir@disroot.org>
2023-05-30 20:57:23 +0400

    Fix cellpicker

--- app/scripts/directives/cellpicker.js
+++ app/scripts/directives/cellpicker.js
@@ -1,6 +1,6 @@
 import template from './cellpicker.html';
 
-function cellPickerDirective(DeviceData, $locale) {
+function cellPickerDirective(DeviceData, $locale, $window) {
   'ngInject';
 
   const DEFAULT_PLACEHOLDER = 'Select a control or search for it...';
@@ -21,10 +21,17 @@ function cellPickerDirective(DeviceData, $locale) {
         if (!cellId)
           return null;
 
-        var cell = DeviceData.proxy(cellId),
-            devName = DeviceData.devices.hasOwnProperty(cell.deviceId) ?
-            DeviceData.devices[cell.deviceId].getName($locale.id) : cell.deviceId,
-            fullCellName = devName + ' / ' + cell.getName($locale.id);
+        const cell = DeviceData.proxy(cellId);
+
+        var devName = cell.deviceId;
+        if (DeviceData.devices.hasOwnProperty(cell.deviceId)) {
+          devName = DeviceData.devices[cell.deviceId].getName($locale.id);
+          const showSystemDevices = $window.localStorage['show-system-devices'] == 'yes';
+          if (!showSystemDevices && DeviceData.devices[cell.deviceId].isSystemDevice) {
+            return null;
+          }
+        }
+        const fullCellName = devName + ' / ' + cell.getName($locale.id);
 
         if (items.hasOwnProperty(cellId))
           items[cellId].name = fullCellName;
