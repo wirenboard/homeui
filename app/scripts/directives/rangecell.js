3d8e1fdcc14837530e1c64d5fedbd43a103c00b7
vdromanov <vdromanov@yahoo.com>
2023-06-13 09:55:27 +0300

    Feature/60274 range cell (#424) (#443)
    * watch cell params in rangecell
    
    * bump version
    
    * remove commented code in rangecell

--- app/scripts/directives/rangecell.js
+++ app/scripts/directives/rangecell.js
@@ -24,35 +24,19 @@ function rangeCellDirective() {
     controllerAs: "rCtrl",
     controller: RangeCellController,
     link: ($scope, element, attr, cellCtrl) => {
-      /*function relayAttr (name, defaultValue) {
-        // Make sure min/max/precision are initialized to a range that is
-        // broad enough for the range control to initialize correctly.
-        // See https://github.com/angular/angular.js/issues/6726
+
+      // Make sure min/max/precision are initialized to a range that is
+      // broad enough for the range control to initialize correctly.
+      // See https://github.com/angular/angular.js/issues/6726
+      function watchAttr (name, defaultValue) {
         $scope.$watch(() => cellCtrl.cell[name], newValue => {
-          element.find("input").attr(name, cellCtrl.cell[name] === null ? defaultValue : cellCtrl.cell[name]);
+          $scope[name] = !newValue? defaultValue : +newValue;
         });
-      }*/
+      }
 
-
-      /*relayAttr("min", DEFAULT_MIN);
-      relayAttr("max", DEFAULT_MAX);
-      relayAttr("step", DEFAULT_STEP);*/
-      // Protect against non-number values that cause error inside Angular.
-      // Can't just use ng-if on input as with value-cell because in this
-      // case relayAttr hack will not work.
-      /*$scope.target = {
-        get value () {
-          var v = $scope.cell.value - 0;
-          return isNaN(v) ? 0 : v;
-        },
-        set value (newValue) {
-          $scope.cell.value = newValue;
-        }
-      };*/
-
-      $scope["min"] = !cellCtrl.cell["min"]? DEFAULT_MIN : +cellCtrl.cell["min"];
-      $scope["max"] = !cellCtrl.cell["max"]? DEFAULT_MAX : +cellCtrl.cell["max"];
-      $scope["step"] = !cellCtrl.cell["step"]? DEFAULT_STEP : +cellCtrl.cell["step"];
+      watchAttr("max", DEFAULT_MAX);
+      watchAttr("min", DEFAULT_MIN);
+      watchAttr("step", DEFAULT_STEP);
 
       $scope.$watch(() => $scope.cell.value, value => {
         $scope._value = isNaN(value)? 0 : value;
