4535c8db7fc997c160abd154615f619a69d1447e
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2023-10-03 10:06:21 +0500

    Fix uploading fit-image
    Fix downloading diagnostic archive simultaneously with uploading fit-image

--- app/scripts/controllers/backupController.js
+++ app/scripts/controllers/backupController.js
@@ -2,11 +2,9 @@ class BackupCtrl {
   constructor($scope) {
     'ngInject';
 
-    $scope.btnVisible = true;
     $scope.btnEnabled = true;
 
     var downloadRootfs = function () {
-      window.location.href = 'fwupdate/download/rootfs';
       $scope.btnEnabled = false;
     };
 
