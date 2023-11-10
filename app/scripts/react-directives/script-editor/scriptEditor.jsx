f7323e6c927bbbf73232a11c5617361b9ce83438
KraPete <86825564+KraPete@users.noreply.github.com>
2023-11-10 14:17:52 +0500

    Preserve focus after saving in rules editor (#484)

--- app/scripts/react-directives/script-editor/scriptEditor.jsx
+++ app/scripts/react-directives/script-editor/scriptEditor.jsx
@@ -1,27 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { observer } from 'mobx-react-lite';
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
 import { EditorSelection } from '@codemirror/state';
 
-const ScriptEditor = observer(({ text, errorLine, onChange }) => {
-  const [scrolled, setScrolled] = useState(false);
+const ScriptEditor = observer(({ text, errorLine, onChange, visible }) => {
+  var cmRef = useRef();
   const refCallback = ref => {
-    if (!scrolled && errorLine != null && ref?.view) {
-      if (errorLine > ref.view.state.doc.lines) {
-        errorLine = ref.view.state.doc.lines;
-      }
-      const line = ref.view.state.doc.line(errorLine);
-      ref.view.dispatch({
-        selection: EditorSelection.single(line.from, line.to),
+    if (ref?.view && !cmRef.current) {
+      cmRef.current = ref.view;
+      cmRef.current.dispatch({
+        selection: EditorSelection.single(0, 0),
         scrollIntoView: true,
       });
-      setScrolled(true);
     }
   };
+  useEffect(() => {
+    if (cmRef?.current) {
+      if (errorLine != null) {
+        if (errorLine > cmRef.current.state.doc.lines) {
+          errorLine = cmRef.current.state.doc.lines;
+        }
+        const line = cmRef.current.state.doc.line(errorLine);
+        cmRef.current.dispatch({
+          selection: EditorSelection.single(line.from, line.from),
+          scrollIntoView: true,
+        });
+      }
+      cmRef.current.focus();
+    }
+  });
 
   return (
-    <div className="script-editor">
+    <div className="script-editor" style={{ display: visible ? '' : 'none' }}>
       <CodeMirror
         ref={refCallback}
         style={{ height: '100%' }}
