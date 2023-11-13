b0a4b3c95d0360971a5974366fda8996e847090a
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2023-11-13 14:36:23 +0500

    Fix GSM connection creation

--- app/scripts/react-directives/network-connections/connectionsStore.js
+++ app/scripts/react-directives/network-connections/connectionsStore.js
@@ -53,6 +53,7 @@ function getNewConnectionData(type, connections) {
       type: type,
       connection_uuid: '',
       connection_id: 'gsm ' + findIndexForNewConnectionName('gsm (\\d+)', connections),
+      'close-by-priority': false,
     };
   }
   return { type: type, connection_uuid: '', connection_id: connection_id };
