88ad5a4ce7dc9c67bbf62f60860011e1ed5107e2
Nikita Maslov <nikita.maslov@wirenboard.ru>
2023-12-22 13:50:55 +0600

    allow nodejs from dev-tools

--- devserver_stretch.sh
+++ devserver_stretch.sh
@@ -7,7 +7,7 @@ docker run -ti --rm --name homeui-stretch --network host -v $(pwd):/build debian
   echo "deb http://archive.debian.org/debian/ stretch main" > /etc/apt/sources.list && \
   echo "deb [trusted=yes] http://deb.wirenboard.com/dev-tools stable main" >> /etc/apt/sources.list && \
   apt-get update && \
-  apt-get install -y nodejs-12 && \
+  apt-get install -y --allow-unauthenticated nodejs-12 && \
   cd /build && \
   npm install && \
   npm run clean && \
