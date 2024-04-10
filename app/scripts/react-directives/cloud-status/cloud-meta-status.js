eeb96e50a454ebeba5c0357a3b285779abaa1abd
lostpoint-ru <67120046+lostpoint-ru@users.noreply.github.com>
2024-04-10 21:23:03 +0500

    Multitenancy support for wb-cloud-agent (#534)
    * add support for multitenant version of wb-cloud-agent

---
+++ app/scripts/react-directives/cloud-status/cloud-meta-status.js
@@ -0,0 +1,35 @@
+'use strict';
+
+import ReactDOM from 'react-dom/client';
+import CreateCloudStatusMetaWidget from './cloudStatusMetaWidget';
+import CloudStatusMetaStore from "./meta-store";
+
+function cloudStatusMetaDirective(mqttClient, whenMqttReady) {
+  'ngInject';
+
+  return {
+    restrict: 'E',
+    scope: {},
+    link(scope, element) {
+      if (scope.root) {
+        scope.root.unmount();
+      }
+
+      scope.store = new CloudStatusMetaStore(mqttClient, whenMqttReady);
+      scope.root = ReactDOM.createRoot(element[0]);
+      scope.root.render(CreateCloudStatusMetaWidget({ store: scope.store }));
+
+      whenMqttReady().then(() => {
+        mqttClient.addStickySubscription('/wb-cloud-agent/providers', msg => {
+          scope.store.updateProviderList(msg.payload);
+        });
+      });
+
+      element.on('$destroy', () => {
+        scope.root.unmount();
+      });
+    },
+  };
+}
+
+export default cloudStatusMetaDirective;
