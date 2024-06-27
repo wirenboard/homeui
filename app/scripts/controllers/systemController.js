0dd38b2d052c6dfb0ee9464d4a9b984e16c11e25
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2024-06-27 13:45:38 +0500

    Place widget on every page

--- app/scripts/controllers/systemController.js
+++ app/scripts/controllers/systemController.js
@@ -3,7 +3,6 @@
 import { setReactLocale } from '../react-directives/locale';
 import cloudStatusMetaDirective from '../react-directives/cloud-status/cloud-meta-status';
 import firmwareUpdateDirective from '../react-directives/firmware-update/firmware-update';
-import expCheckMetaDirective  from '../react-directives/exp-check/exp-check';
 
 function SystemCtrl() {
   'ngInject';
@@ -15,5 +14,4 @@ export default angular
   .module('homeuiApp.system', [])
   .controller('SystemCtrl', SystemCtrl)
   .directive('cloudStatusMetaWidget', cloudStatusMetaDirective)
-  .directive('firmwareUpdateWidget', firmwareUpdateDirective)
-  .directive('expCheckWidget', expCheckMetaDirective);
+  .directive('firmwareUpdateWidget', firmwareUpdateDirective);
