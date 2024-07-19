1aaedfead9ac8502ee715f8e3a918314b73a80e7
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2024-07-19 11:37:02 +0500

    Hide vulnerability warning

--- app/scripts/react-directives/exp-check/store.js
+++ app/scripts/react-directives/exp-check/store.js
@@ -1,18 +1,27 @@
-import { makeAutoObservable } from 'mobx';
+import { makeObservable, observable } from 'mobx';
 
 class ExpCheckStore {
   constructor() {
     this.result = null;
-    this.details = null;
-    makeAutoObservable(this, {}, { autoBind: true });
+    this.details = [];
+    this.timerHandler = null;
+
+    makeObservable(this, { result: observable, details: observable });
   }
 
-  updateResult(result) {
+  update(result, details) {
     this.result = result;
-  }
-
-  updateDetails(details) {
-    this.details = details;
+    this.details = details || [];
+    if (result === 'found') {
+      if (this.timerHandler === null) {
+        this.timerHandler = setInterval(() => fetch('/api/check'), 1000 * 60 * 10);
+      }
+    } else {
+      if (this.timerHandler !== null) {
+        clearInterval(this.timerHandler);
+        this.timerHandler = null;
+      }
+    }
   }
 }
 
