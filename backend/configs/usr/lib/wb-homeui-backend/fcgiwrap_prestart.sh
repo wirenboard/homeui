97c8260d09d709f2abbb9001ad13567a2f6f1447
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2025-02-25 12:39:19 +0500

    Fix uploads dir permissions
    Former version attempted to execute the output of the command rather than checking its exit status.

--- backend/configs/usr/lib/wb-homeui-backend/fcgiwrap_prestart.sh
+++ backend/configs/usr/lib/wb-homeui-backend/fcgiwrap_prestart.sh
@@ -3,9 +3,10 @@
 # nginx user should has rw access to fwupdate uploads dir
 nginx_uploads_dir="/mnt/data/uploads"
 nginx_user="www-data"
-if [[ -d $nginx_uploads_dir ]] && $(id -u $nginx_user > /dev/null 2>&1); then
+
+if [[ -d $nginx_uploads_dir ]] && id -u $nginx_user > /dev/null 2>&1; then
     chown -R $nginx_user:$nginx_user $nginx_uploads_dir
 fi
 
-# grant access fastcgi sripts to mosquitto's unix socket
+# grant access fastcgi scripts to mosquitto's unix socket
 usermod -a -G mosquitto $nginx_user 
