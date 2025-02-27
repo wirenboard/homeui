b12d9d12332afd628299f5170548ceef4973b688
ninedev <master-vva@narod.ru>
2025-02-27 11:20:53 +0300

    update table logic

--- frontend/src/components/table/types.ts
+++ frontend/src/components/table/types.ts
@@ -1,14 +1,18 @@
-export interface TableProps {
+import { HTMLAttributes } from 'react';
+
+export interface TableProps extends HTMLAttributes<HTMLDivElement> {
   isLoading?: boolean;
   isFullWidth?: boolean;
 }
 
-export interface TableRowProps {
+export interface TableRowProps extends HTMLAttributes<HTMLDivElement | HTMLAnchorElement> {
   url?: string;
+  isFullWidth?: boolean;
 }
 
-export interface TableCellProps {
+export interface TableCellProps extends HTMLAttributes<HTMLDivElement> {
+  width?: number;
   visibleOnHover?: boolean;
   preventClick?: boolean;
-  fitContent?: boolean;
+  ellipsis?: boolean;
 }
