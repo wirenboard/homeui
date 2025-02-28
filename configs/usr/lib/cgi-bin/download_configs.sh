04ee41c14587258b3e6420202f559a6c27856676
lostpoint-ru <67120046+lostpoint-ru@users.noreply.github.com>
2025-02-28 10:43:51 +0500

    Fix/limit pigz cores (#162)
    * limit pigz cores to N/2 if N>1

--- configs/usr/lib/cgi-bin/download_configs.sh
+++ configs/usr/lib/cgi-bin/download_configs.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+run_pigz() {
+	# if there are more than 1 core, use half of them
+	cpu_cores=$(nproc --all)
+	if [ $cpu_cores -gt 1 ]; then
+    		pigz -p $(($cpu_cores/2))
+	else
+    		pigz
+	fi
+}
+
 SERIAL=$(cat "/var/lib/wirenboard/short_sn.conf")
 printf -v date '%(%Y%m%d_%H%M)T' -1
 
@@ -11,5 +21,4 @@ echo "Content-Type: application/octet-stream"
 echo ""
 cd "$MOUNT_DIR"
 sudo tar --exclude='/var/tmp' --exclude='/var/log' -cf - \
-	/etc /mnt/data/etc /var /mnt/data/root/zigbee2mqtt/data /mnt/data/makesimple/.SprutHub | pigz
-
+	/etc /mnt/data/etc /var /mnt/data/root/zigbee2mqtt/data /mnt/data/makesimple/.SprutHub | run_pigz
--- configs/usr/lib/cgi-bin/download_rootfs.sh
+++ configs/usr/lib/cgi-bin/download_rootfs.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+run_pigz() {
+	# if there are more than 1 core, use half of them
+	cpu_cores=$(nproc --all)
+	if [ $cpu_cores -gt 1 ]; then
+    		pigz -p $(($cpu_cores/2))
+	else
+    		pigz
+	fi
+}
+
 MOUNT_DIR=$(mktemp -d -t "WB-RFS.XXXXXXX")
 SERIAL=$(cat "/var/lib/wirenboard/short_sn.conf")
 ROOTFS_DEV=$(mount | grep "on / type ext4" | awk '{print $1}')
@@ -27,5 +37,5 @@ else
     echo "Content-Type: application/octet-stream"
     echo ""
     cd "$MOUNT_DIR"
-    sudo tar --exclude='tmp' --exclude='var/tmp' -cf - * | pigz
+    sudo tar --exclude='tmp' --exclude='var/tmp' -cf - * | run_pigz
 fi
