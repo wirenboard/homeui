1f9127c9935536a0bba9a662d6d9a8c14e7c3c85
Victor Vedenin <master-vva@narod.ru>
2025-06-09 11:23:01 +0300

    update react components styles and logic, add edit icon, update page component accessibility (#747)

--- frontend/src/components/dropdown/dropdown.tsx
+++ frontend/src/components/dropdown/dropdown.tsx
@@ -4,25 +4,30 @@ import { DropdownProps } from './types';
 import './styles.css';
 
 export const Dropdown = ({
+  id,
   options,
   className,
   value,
   placeholder,
   size = 'default',
   ariaLabel,
+  isDisabled,
+  isSearchable,
   onChange,
 }: DropdownProps) => (
   <Select
+    inputId={id}
     className={classNames(className, {
       'dropdown-m': size === 'default',
       'dropdown-s': size === 'small',
     })}
     classNamePrefix="dropdown"
     options={options}
-    isClearable={false}
     value={options.find((option) => option.value === value)}
     placeholder={placeholder || ''}
-    isSearchable={false}
+    isDisabled={isDisabled}
+    isSearchable={isSearchable}
+    isClearable={false}
     aria-label={ariaLabel}
     unstyled
     onChange={onChange}
--- frontend/src/components/dropdown/index.ts
+++ frontend/src/components/dropdown/index.ts
@@ -1,1 +1,2 @@
 export { Dropdown } from './dropdown';
+export * from './types';
