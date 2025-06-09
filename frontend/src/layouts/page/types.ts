35c60c700e105fe096c6b7cd862aaba87768c372
ninedev <master-vva@narod.ru>
2025-06-09 15:36:51 +0300

    fix rule name edit empty value

--- frontend/src/layouts/page/types.ts
+++ frontend/src/layouts/page/types.ts
@@ -4,10 +4,11 @@ export interface PageProps {
   title: string;
   isEditingTitle?: boolean;
   editingTitlePlaceholder?: string;
-  onTitleChange?: (_title: string) => void;
   hasRights: boolean;
   isLoading?: boolean;
   stickyHeader?: boolean;
   actions?: ReactElement;
   errors?: { variant?: string; text?: string; code?: number }[];
+  onTitleChange?: (_title: string) => void;
+  onTitleEditEnable?: () => void;
 }
