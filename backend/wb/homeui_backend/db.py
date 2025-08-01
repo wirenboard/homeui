31ef048cba47bb36fed97c51b5591340866c9ee8
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2025-08-01 07:26:12 +0000

    Make codacy happy

--- backend/wb/homeui_backend/db.py
+++ backend/wb/homeui_backend/db.py
@@ -42,7 +42,7 @@ def create_db(db_file: str) -> sqlite3.Connection:
     con = sqlite3.connect(db_file)
     create_tables(con)
     cur = con.cursor()
-    cur.execute(f"PRAGMA user_version = {DB_SCHEMA_VERSION}")
+    cur.execute("PRAGMA user_version = 1")
     return con
 
 
