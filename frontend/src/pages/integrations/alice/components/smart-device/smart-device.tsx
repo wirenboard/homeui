2c70491b0528023eb0997b30d4f7269f6f8d5c27
Vitalii <vitalii.gaponov@wirenboard.com>
2025-09-09 20:48:52 +0300

    fix: smart-device.tsx linter error + error import "This JSX tag requires the module path 'react/jsx-runtime' to exist"

--- frontend/src/pages/integrations/alice/components/smart-device/smart-device.tsx
+++ frontend/src/pages/integrations/alice/components/smart-device/smart-device.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { FormEvent, useCallback, useEffect, useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
@@ -155,7 +156,7 @@ export const SmartDevice = observer(({ id, deviceStore, onSave, onDelete, onOpen
             <Dropdown
               value={data.room_id}
               options={Array.from(rooms.keys()).map((room) => ({ label: rooms.get(room).name, value: room }))}
-              onChange={({ value: room_id }: any) => setData({ ...data, room_id })}
+              onChange={({ value: roomId }: any) => setData({ ...data, room_id: roomId })}
             />
           </label>
 
