928b7e404f235d4f843856adc3d15d0207ec77d8
Vitalii <vitalii.gaponov@wirenboard.com>
2025-10-17 20:35:18 +0300

    refactor: add comment

--- frontend/src/pages/integrations/alice/components/room/room.tsx
+++ frontend/src/pages/integrations/alice/components/room/room.tsx
@@ -52,6 +52,7 @@ export const Room = observer(({ id, onOpenDevice, onSave, onDelete }: RoomParams
     if (id === 'all') {
       return formatRoomDevices(Array.from(devices.keys()));
     } else if (id) {
+      // After create new room - can return undefined, fix it by []
       return formatRoomDevices(rooms.get(id)?.devices || []);
     } else {
       return [];
