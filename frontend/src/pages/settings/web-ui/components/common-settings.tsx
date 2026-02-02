4a76424b514d294ecad4ab65fcf12bcacec496d2
ninedev <master-vva@narod.ru>
2026-02-02 17:24:00 +0300

    add save button to web ui

--- frontend/src/pages/settings/web-ui/components/common-settings.tsx
+++ frontend/src/pages/settings/web-ui/components/common-settings.tsx
@@ -1,9 +1,9 @@
-import debounce from 'lodash/debounce';
 import { observer } from 'mobx-react-lite';
-import { useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Button } from '@/components/button';
 import { type Option } from '@/components/dropdown';
-import { BooleanField, FormFieldGroup, OptionsField, StringField } from '@/components/form';
+import { BooleanField, FormButtonGroup, FormFieldGroup, OptionsField, StringField } from '@/components/form';
 import { authStore, UserRole } from '@/stores/auth';
 import { type CommonSettingsProps } from '../types';
 
@@ -11,59 +11,65 @@ const CommonSettings = observer(({ onChangeLanguage, dashboardsStore }: CommonSe
   const { t } = useTranslation();
   const [showSystemDevices, setShowSystemDevices] = useState((localStorage['show-system-devices'] || 'no') === 'yes');
   const [language, setLanguage] = useState(localStorage.getItem('language') || 'en');
+  const [defaultDashboardId, setDefaultDashboardId] = useState('');
+  const [description, setDescription] = useState('');
   const options = dashboardsStore.dashboardsList
     .filter((dashboard) => !dashboard.options.isHidden)
     .map((dashboard) => ({ label: dashboard.name, value: dashboard.id }));
 
-  const setShowSystemDevicesHandler = (value: boolean) => {
-    localStorage.setItem('show-system-devices', value ? 'yes' : 'no');
-    setShowSystemDevices(value);
-  };
-
-  const onChangeLanguageHandler = (lang: string) => {
-    localStorage.setItem('language', lang);
-    onChangeLanguage(lang);
-    setLanguage(lang);
-  };
+  useEffect(() => {
+    setDescription(dashboardsStore.description);
+    setDefaultDashboardId(dashboardsStore.defaultDashboardId);
+  }, [dashboardsStore.description, dashboardsStore.defaultDashboardId]);
 
   const languageOptions: Option<string>[] = [
     { label: 'English', value: 'en' },
     { label: 'Русский', value: 'ru' },
   ];
 
-  const debouncedNameChange = useMemo(
-    () => debounce((value: string) => dashboardsStore.setDescription(value), 1000),
-    []
-  );
+  const applyHandler = () => {
+    localStorage.setItem('show-system-devices', showSystemDevices ? 'yes' : 'no');
+    localStorage.setItem('language', language);
+    onChangeLanguage(language);
+    dashboardsStore.setDefaultDashboardId(defaultDashboardId);
+    dashboardsStore.setDescription(description);
+  };
 
   return (
     <FormFieldGroup heading={t('web-ui-settings.labels.common-settings')}>
       <OptionsField
         title={t('web-ui-settings.labels.default-dashboard')}
-        value={dashboardsStore.defaultDashboardId}
+        value={defaultDashboardId}
         options={options}
         isDisabled={dashboardsStore.isLoading}
-        onChange={(id: string) => dashboardsStore.setDefaultDashboardId(id)}
+        onChange={setDefaultDashboardId}
       />
       <OptionsField
         title={t('web-ui-settings.labels.language')}
         value={language}
         options={languageOptions}
-        onChange={onChangeLanguageHandler}
+        onChange={setLanguage}
       />
       {authStore.hasRights(UserRole.Operator) && (
         <StringField
           title={t('web-ui-settings.labels.name')}
           description={t('web-ui-settings.labels.name-description')}
-          value={dashboardsStore.description}
-          onChange={debouncedNameChange}
+          value={description}
+          onChange={setDescription}
         />
       )}
       <BooleanField
         title={t('web-ui-settings.labels.show-system-devices')}
         value={showSystemDevices}
-        onChange={setShowSystemDevicesHandler}
+        onChange={setShowSystemDevices}
       />
+
+      <FormButtonGroup>
+        <Button
+          label={t('common.buttons.apply')}
+          onClick={applyHandler}
+        />
+      </FormButtonGroup>
     </FormFieldGroup>
   );
 });
