bdc6937b6a375be3a52a6817f2b860ba0844995e
Victor Vedenin <master-vva@narod.ru>
2026-05-05 18:36:33 +0300

    fix dropdown opening on touch (#1073)

--- frontend/app/scripts/controllers/homeController.js
+++ frontend/app/scripts/controllers/homeController.js
@@ -1,22 +1,34 @@
-import { reaction } from 'mobx';
+import { when } from 'mobx';
 
 export default class HomeCtrl {
   constructor($state, $rootScope, rolesFactory) {
     'ngInject';
     this.roles = rolesFactory;
 
-    reaction(() => $rootScope.dashboardsStore.isLoading, () => {
-      const { defaultDashboardId, dashboards } = $rootScope.dashboardsStore
+    const store = $rootScope.dashboardsStore;
+
+    const redirect = () => {
+      const { defaultDashboardId, dashboards } = store;
+
       if (defaultDashboardId && dashboards.get(defaultDashboardId)) {
         const dashboard = dashboards.get(defaultDashboardId);
-        if (dashboard.isSvg) {
-          $state.go('dashboard-svg', { id: defaultDashboardId });
-        } else {
-          $state.go('dashboard', { id: defaultDashboardId});
-        }
+
+        $state.go(
+          dashboard.isSvg ? 'dashboard-svg' : 'dashboard',
+          { id: defaultDashboardId }
+        );
       } else {
         $state.go('dashboards');
       }
-    });
+    };
+
+    if (!store.isLoading) {
+      redirect();
+    } else {
+      when(
+        () => !store.isLoading,
+        redirect
+      );
+    }
   }
 }
