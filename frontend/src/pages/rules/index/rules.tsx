3d8b10daef2a0c57b1a12c2c1a4a5944e0dfad4e
Victor Vedenin <master-vva@narod.ru>
2026-06-25 18:18:50 +0300

    fix the rule's opening with slashes (#1133)

--- frontend/src/pages/rules/index/rules.tsx
+++ frontend/src/pages/rules/index/rules.tsx
@@ -67,7 +67,7 @@ const RulesPage = observer(() => {
       <Table isLoading={isRulesUpdating}>
         {rules.map((rule) => (
           <TableRow
-            url={`/rules/edit/${rule.virtualPath}`}
+            url={`/rules/${rule.virtualPath}`}
             aria-label={t('rules.labels.open-rule', { path: rule.virtualPath })}
             key={rule.virtualPath}
           >
