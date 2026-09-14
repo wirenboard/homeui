4c1356f015b8e9f2b1538b3e3968d32e21fd669f
Nikolay Korotkiy <sikmir@disroot.org>
2026-09-14 10:26:44 +0400

    Reject uploaded custom-font files whose content does not match their extension

--- backend/tests/fonts_test.py
+++ backend/tests/fonts_test.py
@@ -5,6 +5,13 @@ import unittest
 
 from wb.homeui_backend.fonts import FontsStore
 
+FONT_DATA_BY_EXTENSION = {
+    ".ttf": b"\x00\x01\x00\x00ttf-data",
+    ".otf": b"OTTOotf-data",
+    ".woff": b"wOFFwoff-data",
+    ".woff2": b"wOF2woff2-data",
+}
+
 
 class FontsStoreTest(unittest.TestCase):
     def setUp(self):
@@ -17,26 +24,36 @@ class FontsStoreTest(unittest.TestCase):
         self.assertEqual(self.store.list_fonts(), [])
 
     def test_save_and_list(self):
-        self.store.save_font("MyFont.ttf", b"fake-ttf-data")
+        self.store.save_font("MyFont.ttf", FONT_DATA_BY_EXTENSION[".ttf"])
         fonts = self.store.list_fonts()
         self.assertEqual(len(fonts), 1)
         self.assertEqual(fonts[0]["name"], "MyFont.ttf")
-        self.assertEqual(fonts[0]["size"], len(b"fake-ttf-data"))
+        self.assertEqual(fonts[0]["size"], len(FONT_DATA_BY_EXTENSION[".ttf"]))
 
     def test_save_returns_info(self):
-        result = self.store.save_font("Test.woff2", b"woff2-data")
-        self.assertEqual(result, {"name": "Test.woff2", "size": len(b"woff2-data")})
+        result = self.store.save_font("Test.woff2", FONT_DATA_BY_EXTENSION[".woff2"])
+        self.assertEqual(result, {"name": "Test.woff2", "size": len(FONT_DATA_BY_EXTENSION[".woff2"])})
 
     def test_save_bad_extension(self):
         with self.assertRaises(ValueError):
             self.store.save_font("bad.txt", b"data")
 
     def test_save_accepts_all_allowed_extensions(self):
-        for ext in (".ttf", ".woff", ".woff2", ".otf"):
-            self.store.save_font(f"font{ext}", b"data")
+        for ext, data in FONT_DATA_BY_EXTENSION.items():
+            self.store.save_font(f"font{ext}", data)
+
+    def test_save_rejects_non_font_content(self):
+        with self.assertRaisesRegex(ValueError, "File content does not match font extension: .ttf"):
+            self.store.save_font("NotAFont.ttf", b"not-font-data")
+        self.assertEqual(self.store.list_fonts(), [])
+
+    def test_save_rejects_font_content_with_mismatched_extension(self):
+        with self.assertRaisesRegex(ValueError, "File content does not match font extension: .ttf"):
+            self.store.save_font("WrongFormat.ttf", FONT_DATA_BY_EXTENSION[".woff"])
+        self.assertEqual(self.store.list_fonts(), [])
 
     def test_delete_existing(self):
-        self.store.save_font("ToDelete.ttf", b"data")
+        self.store.save_font("ToDelete.ttf", FONT_DATA_BY_EXTENSION[".ttf"])
         self.assertTrue(self.store.delete_font("ToDelete.ttf"))
         self.assertEqual(self.store.list_fonts(), [])
 
@@ -44,16 +61,16 @@ class FontsStoreTest(unittest.TestCase):
         self.assertFalse(self.store.delete_font("NoSuchFont.ttf"))
 
     def test_overwrite(self):
-        self.store.save_font("Font.ttf", b"old")
-        self.store.save_font("Font.ttf", b"new-data")
+        self.store.save_font("Font.ttf", b"\x00\x01\x00\x00old")
+        self.store.save_font("Font.ttf", b"\x00\x01\x00\x00new-data")
         fonts = self.store.list_fonts()
         self.assertEqual(len(fonts), 1)
-        self.assertEqual(fonts[0]["size"], len(b"new-data"))
+        self.assertEqual(fonts[0]["size"], len(b"\x00\x01\x00\x00new-data"))
 
     def test_list_sorted_by_name(self):
-        self.store.save_font("Zebra.ttf", b"z")
-        self.store.save_font("Alpha.woff", b"a")
-        self.store.save_font("Middle.otf", b"m")
+        self.store.save_font("Zebra.ttf", FONT_DATA_BY_EXTENSION[".ttf"])
+        self.store.save_font("Alpha.woff", FONT_DATA_BY_EXTENSION[".woff"])
+        self.store.save_font("Middle.otf", FONT_DATA_BY_EXTENSION[".otf"])
         names = [f["name"] for f in self.store.list_fonts()]
         self.assertEqual(names, ["Alpha.woff", "Middle.otf", "Zebra.ttf"])
 
